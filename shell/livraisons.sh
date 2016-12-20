a7b3b8a633c1fb00dc0859c72f136f28104b8a13
NG38DCD <stephane.dewitte.external@airbus.com>
2016-12-20 15:07:52 +0100

    Livraison automatique OK

--- shell/livraisons.sh
+++ shell/livraisons.sh
@@ -3,6 +3,7 @@ source `dirname $0`/env
 for f in `find $livroot -type f| grep -v "initial_import.sql"|sort`; do
   livexist=`$shellroot/livraison_exists.sh $(basename $f)`
   if [ "$livexist" -eq "0" ];then
+      echo "LIVRAIONS A TRAITER : $f"
       $psql -A -q -t -d delain -U webdelain -f $f
       $psql -A -q -t -d delain -U webdelain << EOF
 insert into livraisons (liv_fichier) values ('$(basename $f)');
