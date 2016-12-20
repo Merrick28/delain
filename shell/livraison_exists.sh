c9c60571ab58d4c9de8b9fa3050ed2257ad394e7
NG38DCD <stephane.dewitte.external@airbus.com>
2016-12-20 14:41:19 +0100

    Debut livraisons automatiques

--- shell/livraison_exists.sh
+++ shell/livraison_exists.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source `dirname $0`/env
 $psql -q -t -d delain -U webdelain << EOF
-select coalesce(liv_fichier,'F') from livraisons where liv_fichier = '$1';
+select count(*) from livraisons where liv_fichier = '$1';
 EOF
