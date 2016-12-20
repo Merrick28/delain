da8012d28dfb1d463f524ac3f10ee57091b37f52
NG38DCD <stephane.dewitte.external@airbus.com>
2016-12-20 15:02:45 +0100

    Shell livraisons automatiques

--- shell/livraisons.sh
+++ shell/livraisons.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 source `dirname $0`/env
 for f in `find $livroot -type f -maxdepth 1| grep -v "initial_import.sql"`; do
-  livexist=`$shellroot/livraison_exists.sh $f
+  livexist=`$shellroot/livraison_exists.sh $(basename $f)`
   if [ "$livexist" -eq "0" ];then
     echo "$f déjà traité"
   else
-    echo "$f a traiter"
+    $psql -A -q -t -d delain -U webdelain -f $f
+    $psql -A -q -t -d delain -U webdelain << EOF
+insert into livraisons (liv_fichier) values ($(basename $f));
+EOF
   fi
 done
