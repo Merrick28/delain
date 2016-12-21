e445a0aba70a9bdaf631bf4b467e2fd5abd093dd
NG38DCD <stephane.dewitte.external@airbus.com>
2016-12-21 14:54:18 +0100

    Mise en forme du code

--- web/www/api/game/menu.php
+++ web/www/api/game/menu.php
@@ -23,11 +23,11 @@ if (!function_exists('apc_fetch'))
     }
 }
 
-$type_auth     = $_REQUEST['type_auth'];
-$cle_connect   = $_REQUEST['cle_connect'];
-$compt_cod     = $_REQUEST['compt_cod'];
+$type_auth = $_REQUEST['type_auth'];
+$cle_connect = $_REQUEST['cle_connect'];
+$compt_cod = $_REQUEST['compt_cod'];
 $ext_perso_cod = $_REQUEST['ext_perso_cod'];
-$typesort      = $_REQUEST['typesort'];
+$typesort = $_REQUEST['typesort'];
 
 require_once G_CHE . "jeu_test/verif_connexion.php";
 
@@ -35,7 +35,7 @@ $pa_dep = $db->get_pa_dep($perso_cod);
 
 // on commence à aller chercher les infos
 $is_intangible = $db->is_intangible($perso_cod);
-$intangible    = 0;
+$intangible = 0;
 if ($is_intangible)
 {
     $intangible = 1;
@@ -43,7 +43,7 @@ if ($is_intangible)
 
 
 $is_enlumineur = $db->is_enlumineur($perso_cod);
-$enlumineur    = 0;
+$enlumineur = 0;
 if ($is_enlumineur)
 {
     $enlumineur = 1;
@@ -51,23 +51,22 @@ if ($is_enlumineur)
 
 
 $is_refuge = $db->is_refuge($perso_cod);
-$refuge    = 0;
+$refuge = 0;
 if ($is_refuge)
 {
     $refuge = 1;
 }
 
 
-
 $is_milice = $db->is_milice($perso_cod);
-$milice    = 0;
+$milice = 0;
 if ($is_milice)
 {
     $milice = 1;
 }
 
 
-$req    = "select mger_perso_cod from magasin_gerant where mger_perso_cod = $perso_cod ";
+$req = "select mger_perso_cod from magasin_gerant where mger_perso_cod = $perso_cod ";
 $db->query($req);
 $gerant = 'N';
 if ($db->nf() != 0)
@@ -75,7 +74,7 @@ if ($db->nf() != 0)
     $gerant = 'O';
 }
 
-$req        = "select dper_niveau from dieu_perso where dper_perso_cod = $perso_cod and dper_niveau > 3";
+$req = "select dper_niveau from dieu_perso where dper_perso_cod = $perso_cod and dper_niveau > 3";
 $db->query($req);
 $admin_dieu = 'N';
 if ($db->nf() != 0)
@@ -83,7 +82,7 @@ if ($db->nf() != 0)
     $admin_dieu = 'O';
 }
 
-$req           = "select tfid_perso_cod from temple_fidele where tfid_perso_cod = $perso_cod ";
+$req = "select tfid_perso_cod from temple_fidele where tfid_perso_cod = $perso_cod ";
 $db->query($req);
 $fidele_gerant = 'N';
 if ($db->nf() != 0)
@@ -99,23 +98,23 @@ $req = "select perso_pa,perso_nom,perso_niveau_vampire,perso_admin_echoppe,perso
 $db->query($req);
 $db->next_record();
 
-$pa                 = $db->f("perso_pa");
-$nom_perso          = $db->f("perso_nom");
-$energie            = $db->f("perso_energie");
-$armure_perso       = $db->f("armure_perso");
-$degats_perso       = $db->f("degats_perso");
-$det_deg            = explode(";", $degats_perso);
-$px_actuel          = $db->f("perso_px");
-$px_limite          = $db->f("limite_niveau");
-$niveau_xp          = ($db->f("perso_px") - $db->f("limite"));
-$div_xp             = ($db->f("limite_niveau") - $db->f("limite"));
-$niveau_xp          = $niveau_xp / $div_xp;
-$pv                 = $db->f("perso_pv");
-$px                 = $db->f("perso_px");
-$pv_max             = $db->f("perso_pv_max");
-$admin_echoppe      = $db->f("perso_admin_echoppe");
+$pa = $db->f("perso_pa");
+$nom_perso = $db->f("perso_nom");
+$energie = $db->f("perso_energie");
+$armure_perso = $db->f("armure_perso");
+$degats_perso = $db->f("degats_perso");
+$det_deg = explode(";", $degats_perso);
+$px_actuel = $db->f("perso_px");
+$px_limite = $db->f("limite_niveau");
+$niveau_xp = ($db->f("perso_px") - $db->f("limite"));
+$div_xp = ($db->f("limite_niveau") - $db->f("limite"));
+$niveau_xp = $niveau_xp / $div_xp;
+$pv = $db->f("perso_pv");
+$px = $db->f("perso_px");
+$pv_max = $db->f("perso_pv_max");
+$admin_echoppe = $db->f("perso_admin_echoppe");
 $admin_echoppe_noir = $db->f("perso_admin_echoppe_noir");
-$is_vampire         = $db->f("perso_niveau_vampire");
+$is_vampire = $db->f("perso_niveau_vampire");
 if ($db->f("perso_pv_max") == 0)
 {
     $hp = 0;
@@ -126,25 +125,24 @@ else
 }
 $is_fam = 0;
 if ($db->f("perso_type_perso") == 3)
-    
 {
     $is_fam = 1;
 }
 
 
 // énergie du familier divin
-$is_fam_divin   = 0;
+$is_fam_divin = 0;
 $energie_divine = -1;
-$barre_divine   = -1;
+$barre_divine = -1;
 if ($db->f("perso_gmon_cod") == 441)
 {
-    $is_fam_divin   = 1;
-    $db_divin       = new base_delain;
-    $req            = "select dper_points from dieu_perso where dper_perso_cod = $perso_cod";
+    $is_fam_divin = 1;
+    $db_divin = new base_delain;
+    $req = "select dper_points from dieu_perso where dper_perso_cod = $perso_cod";
     $db_divin->query($req);
     $db_divin->next_record();
     $energie_divine = $db_divin->f("dper_points");
-    $barre_divine   = floor(($energie_divine / 200) * 10) * 10;
+    $barre_divine = floor(($energie_divine / 200) * 10) * 10;
     if ($barre_divine >= 100)
     {
         $barre_divine = 100;
@@ -156,7 +154,6 @@ if ($px_actuel >= $px_limite)
 {
     $passage_niveau = 1;
 }
-
 
 
 $barre_hp = '0';
@@ -248,21 +245,21 @@ if ($niveau_xp >= 1)
 }
 
 // position
-$tab            = $db->get_pos($perso_cod);
-$position_x     = $tab['x'];
-$position_y     = $tab['y'];
+$tab = $db->get_pos($perso_cod);
+$position_x = $tab['x'];
+$position_y = $tab['y'];
 $position_etage = $tab['etage_libelle'];
 
 // enchanteur
-$enchanteur    = 0;
+$enchanteur = 0;
 $barre_energie = 0;
 $perso_energie = 0;
 $is_enchanteur = $db->is_enchanteur($perso_cod);
 if ($is_enchanteur)
 {
-    $enchanteur    = 1;
-    $hp            = 0;
-    $hp            = $energie / 100;
+    $enchanteur = 1;
+    $hp = 0;
+    $hp = $energie / 100;
     $barre_energie = '0';
     if ($hp >= 0.1)
     {
@@ -316,18 +313,18 @@ if ($is_perso_quete)
 
 
 // lieux
-$lieu     = 0;
+$lieu = 0;
 $nom_lieu = 0;
-$libelle  = 0;
-$is_lieu  = $db->is_lieu($perso_cod);
+$libelle = 0;
+$is_lieu = $db->is_lieu($perso_cod);
 if ($is_lieu)
 {
-    $lieu     = 1;
+    $lieu = 1;
     $tab_lieu = $db->get_lieu($perso_cod);
     if ($tab_lieu['url'] != null)
     {
         $nom_lieu = $tab_lieu['nom'];
-        $libelle  = (!empty($tab_lieu['evo_libelle']) ) ? $tab_lieu['evo_libelle'] : $tab_lieu['libelle'];
+        $libelle = (!empty($tab_lieu['evo_libelle'])) ? $tab_lieu['evo_libelle'] : $tab_lieu['libelle'];
     }
 }
 
@@ -340,12 +337,11 @@ $nb_mess = $db->f('nombre');
 
 // locké
 $is_locked = $db->is_locked($perso_cod);
-$locke     = 0;
+$locke = 0;
 if ($is_locked)
 {
     $locke = 1;
 }
-
 
 
 // ramasser
@@ -363,14 +359,14 @@ if (($db->nb_obj_sur_case($perso_cod) != 0) || ($db->nb_or_sur_case($perso_cod))
         $pa_ramasse = $db->getparm_n(41);
     }
 }
-$req_tran    = "select * from transaction where (tran_vendeur = $perso_cod or tran_acheteur = $perso_cod)";
+$req_tran = "select * from transaction where (tran_vendeur = $perso_cod or tran_acheteur = $perso_cod)";
 $db->query($req_tran);
 $transaction = $db->nf();
 
 // potions
 $req2 = "select pcomp_pcomp_cod from perso_competences where pcomp_pcomp_cod in (97,100,101) and pcomp_perso_cod = $perso_cod";
 $db->query($req2);
- $potions = 0;
+$potions = 0;
 if ($db->nf() != 0)
 {
     $potions = 1;
@@ -386,62 +382,62 @@ if ($db->nf() != 0)
     $religion = 1;
 }
 
-$data   = array(
-   array('name' => 'nom', 'valeur' => $perso_nom),
-   array('name' => 'pa', 'valeur' => $pa),
-   array('name' => 'is_fam', 'valeur' => $is_fam),
-   array('name' => 'barre_hp', 'valeur' => $barre_hp),
-   array('name' => 'pv', 'valeur' => $pv),
-   array('name' => 'pv_max', 'valeur' => $pv_max),
-   array('name' => 'barre_xp', 'valeur' => $barre_xp),
-   array('name' => 'perso_px', 'valeur' => $px),
-   array('name' => 'armure', 'valeur' => $armure_perso),
-   array('name' => 'degats', 'valeur' => $det_deg[0] . '-' . $det_deg[1]),
-   array('name' => 'etage', 'valeur' => $position_etage),
-   array('name' => 'posx', 'valeur' => $position_x),
-   array('name' => 'posy', 'valeur' => $position_y),
-   array('name' => 'passage_niveau', 'valeur' => $passage_niveau),
-   array('name' => 'enchanteur', 'valeur' => $enchanteur),
-   array('name' => 'barre_energie', 'valeur' => $barre_energie),
-   array('name' => 'perso_energie', 'valeur' => $energie),
-   array('name' => 'quete', 'valeur' => $quete),
-   array('name' => 'intangible', 'valeur' => $intangible),
-   array('name' => 'lieu', 'valeur' => $lieu),
-   array('name' => 'nom_lieu', 'valeur' => str_replace("&", "&amp;", $nom_lieu)),
-   array('name' => 'desc_lieu', 'valeur' => $libelle),
-   array('name' => 'nb_mess', 'valeur' => $nb_mess),
-   array('name' => 'lock', 'valeur' => $locke),
-   array('name' => 'pa_dep', 'valeur' => $pa_dep),
-   array('name' => 'pa_ramasse', 'valeur' => $pa_ramasse),
-   array('name' => 'objet_case', 'valeur' => $objet_case),
-   array('name' => 'transaction', 'valeur' => $transaction),
-   array('name' => 'prochain_niveau', 'valeur' => $px_limite),
-   array('name' => 'refuge', 'valeur' => $refuge),
-   array('name' => 'milice', 'valeur' => $milice),
-   array('name' => 'fidele_gerant', 'valeur' => $fidele_gerant),
-   array('name' => 'gerant', 'valeur' => $gerant),
-   array('name' => 'admin_echoppe', 'valeur' => $admin_echoppe),
-   array('name' => 'admin_echoppe_noir', 'valeur' => $admin_echoppe_noir),
-   array('name' => 'is_vampire', 'valeur' => $is_vampire),
-   array('name' => 'religion', 'valeur' => $religion),
-   array('name' => 'admin_dieu', 'valeur' => $admin_dieu),
-   array('name' => 'potions', 'valeur' => $potions),
-   array('name' => 'enlumineur', 'valeur' => $enlumineur),
-   array('name' => 'is_fam_divin', 'valeur' => $is_fam_divin),
-   array('name' => 'energie_divine', 'valeur' => $energie_divine),
-   array('name' => 'barre_divine', 'valeur' => $barre_divine)
+$data = array(
+    array('name' => 'nom', 'valeur' => $perso_nom),
+    array('name' => 'pa', 'valeur' => $pa),
+    array('name' => 'is_fam', 'valeur' => $is_fam),
+    array('name' => 'barre_hp', 'valeur' => $barre_hp),
+    array('name' => 'pv', 'valeur' => $pv),
+    array('name' => 'pv_max', 'valeur' => $pv_max),
+    array('name' => 'barre_xp', 'valeur' => $barre_xp),
+    array('name' => 'perso_px', 'valeur' => $px),
+    array('name' => 'armure', 'valeur' => $armure_perso),
+    array('name' => 'degats', 'valeur' => $det_deg[0] . '-' . $det_deg[1]),
+    array('name' => 'etage', 'valeur' => $position_etage),
+    array('name' => 'posx', 'valeur' => $position_x),
+    array('name' => 'posy', 'valeur' => $position_y),
+    array('name' => 'passage_niveau', 'valeur' => $passage_niveau),
+    array('name' => 'enchanteur', 'valeur' => $enchanteur),
+    array('name' => 'barre_energie', 'valeur' => $barre_energie),
+    array('name' => 'perso_energie', 'valeur' => $energie),
+    array('name' => 'quete', 'valeur' => $quete),
+    array('name' => 'intangible', 'valeur' => $intangible),
+    array('name' => 'lieu', 'valeur' => $lieu),
+    array('name' => 'nom_lieu', 'valeur' => str_replace("&", "&amp;", $nom_lieu)),
+    array('name' => 'desc_lieu', 'valeur' => $libelle),
+    array('name' => 'nb_mess', 'valeur' => $nb_mess),
+    array('name' => 'lock', 'valeur' => $locke),
+    array('name' => 'pa_dep', 'valeur' => $pa_dep),
+    array('name' => 'pa_ramasse', 'valeur' => $pa_ramasse),
+    array('name' => 'objet_case', 'valeur' => $objet_case),
+    array('name' => 'transaction', 'valeur' => $transaction),
+    array('name' => 'prochain_niveau', 'valeur' => $px_limite),
+    array('name' => 'refuge', 'valeur' => $refuge),
+    array('name' => 'milice', 'valeur' => $milice),
+    array('name' => 'fidele_gerant', 'valeur' => $fidele_gerant),
+    array('name' => 'gerant', 'valeur' => $gerant),
+    array('name' => 'admin_echoppe', 'valeur' => $admin_echoppe),
+    array('name' => 'admin_echoppe_noir', 'valeur' => $admin_echoppe_noir),
+    array('name' => 'is_vampire', 'valeur' => $is_vampire),
+    array('name' => 'religion', 'valeur' => $religion),
+    array('name' => 'admin_dieu', 'valeur' => $admin_dieu),
+    array('name' => 'potions', 'valeur' => $potions),
+    array('name' => 'enlumineur', 'valeur' => $enlumineur),
+    array('name' => 'is_fam_divin', 'valeur' => $is_fam_divin),
+    array('name' => 'energie_divine', 'valeur' => $energie_divine),
+    array('name' => 'barre_divine', 'valeur' => $barre_divine)
 );
 // à partir d'ici, on a toutes les infos de base
 // on peut partir à la recherche de quelques infos particulières, qui ne seront pas systématiquement affichées
 // on passe au template 
-require G_CHE ."/../includes/Smarty/Smarty.class.php";
+require G_CHE . "/../includes/Smarty/Smarty.class.php";
 $smarty = new Smarty();
 $smarty->assign('data', $data);
 $smarty->assign('type', "perso");
 
 $smarty->template_dir = G_CHE . '/../api/game/template';
-$smarty->compile_dir  = G_CHE . '/../api/compile';
-$smarty->cache_dir    = G_CHE . '/../api/cache';
+$smarty->compile_dir = G_CHE . '/../api/compile';
+$smarty->cache_dir = G_CHE . '/../api/cache';
 $smarty->assign('variable', '<b>Contenu de ma variable</b>');
 if (!isset($typesort))
 {
@@ -460,6 +456,3 @@ switch ($typesort)
         $smarty->display('gen_json.tpl');
         break;
 }
-
-//require '/home/delain/public_html/www/includes/delain_footer.php';
-?>
