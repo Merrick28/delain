919c68a1630152752c90aa08bc30f7d7b46291de
Merrick28 <stephane.dewitte@gmail.com>
2017-12-27 13:02:38 +0100

    Début travail sur templates -> faq terminé

--- web/www/classement_v2.php
+++ web/www/classement_v2.php
@@ -2,6 +2,8 @@
 $perso = new perso;
 $joueurActifs = $perso->getPersosActifs();
 
+print_r($joueurActifs);
+
 $template = $twig->load('classement_v2.twig');
 $options_twig = array(
     'TAB_JOUEUR'  => $joueurActifs
