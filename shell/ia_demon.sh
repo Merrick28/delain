94603fe68ffa6a21700f39cd0a15805205868229
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2018-02-02 11:35:38 +0100

    Update ia_demon.sh

--- shell/ia_demon.sh
+++ shell/ia_demon.sh
@@ -5,11 +5,11 @@ while true
 do
 if [ 7 -le `cat /proc/loadavg | awk '{print $1}' | awk -F "." '{print $1}'` ]
 then
-echo "`date` : Charge systeme trop elevee au lancement général" >> $logdir/ia_auto.log 2>&1
+echo "`date` : Charge systeme trop elevee au lancement général" >> $logdir/ia_auto.log 2>$logfir/ia_auto_error.log
 else
-echo "`date` : Debut du traitement" >> $logdir/ia_auto.log  2>&1
-$shellroot/liste_monstre.sh  >> $logdir/ia_auto.log  2>&1
-$shellroot/ia_boucle.sh >> $logdir/ia_auto.log  2>&1
+echo "`date` : Debut du traitement" >> $logdir/ia_auto.log  2>$logfir/ia_auto_error.log
+$shellroot/liste_monstre.sh  >> $logdir/ia_auto.log  2>$logfir/ia_auto_error.log
+$shellroot/ia_boucle.sh >> $logdir/ia_auto.log  2>$logfir/ia_auto_error.log
 fi
 sleep 10
 done
