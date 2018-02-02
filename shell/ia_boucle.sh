3a7588127ae9919e13ecc48024ad03a5555c1149
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2018-02-02 11:35:17 +0100

    Update ia_boucle.sh

--- shell/ia_boucle.sh
+++ shell/ia_boucle.sh
@@ -32,7 +32,7 @@ then
 num_skip=`expr $num_skip + 1` 
 else
 num_traite=`expr $num_traite + 1` 
-$psql -U webdelain -d delain << EOF >> $logdir/ia_auto.log 2>&1
+$psql -U webdelain -d delain << EOF >> $logdir/ia_auto.log 2>$logfir/ia_auto_error.log
 \timing
 select ia_monstre($monstre);
 EOF
