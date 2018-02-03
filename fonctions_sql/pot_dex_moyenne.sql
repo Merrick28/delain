02b08603d2673255ef6a5098cee42eca9deaf63a
Merrick28 <stephane.dewitte@gmail.com>
2018-02-03 11:12:56 +0100

    Debut refactor fonctions - 2

---
+++ fonctions_sql/pot_dex_moyenne.sql
@@ -0,0 +1,50 @@
+
+--
+-- Name: pot_dex_moyenne(integer); Type: FUNCTION; Schema: potions; Owner: delain
+--
+
+CREATE or REPLACE FUNCTION potions.pot_dex_moyenne(integer) RETURNS text
+LANGUAGE plpgsql
+AS $_$/*********************************************************/
+/* function pot_force_moyenne                            */
+/* parametres :                                          */
+/*  $1 = personnage qui boit la potion                   */
+/* Sortie :                                              */
+/*  code_retour = texte exploitable par php              */
+/*********************************************************/
+/**************************************************************/
+/* 	       			                              */
+/**************************************************************/
+declare
+  personnage alias for $1;	-- perso_cod
+  code_retour text;				-- code retour
+  v_gobj_cod integer;			-- code de l'objet générique
+  duree integer;	-- Duree de l'effet
+  effet integer;	-- Force de l'effet
+
+
+
+begin
+  /*********************************************************/
+  /*        I N I T I A L I S A T I O N S                  */
+  /*********************************************************/
+  v_gobj_cod := 528;
+  code_retour := '';
+  /*Partie générique pour toutes les potions*/
+  select into code_retour potions.potion_generique(personnage,v_gobj_cod);
+  if not found then
+    code_retour := code_retour||'Erreur ! Fonction générique non trouvée ';
+  elsif substring(code_retour from 1 for 6) = 'Erreur' then
+    return code_retour;
+  /*Tous les controles sont OK, on passe alors aux effets de la potion uniquement*/
+  else
+    duree := lancer_des(1,10) + 23;
+    effet := lancer_des(1,3) + 1;
+    perform f_modif_carac(personnage,'DEX',duree,effet);
+    code_retour := code_retour||'<br>Vous aviez toujours rêvé de mettre dans le mille ! Profitez en avant que vos adversaires ne s''en aperçoivent ';
+  end if;
+  return code_retour;
+end;	$_$;
+
+
+ALTER FUNCTION potions.pot_dex_moyenne(integer) OWNER TO delain;
---
+++ fonctions_sql/pot_force_forte.sql
@@ -0,0 +1,52 @@
+--
+-- Name: pot_force_forte(integer); Type: FUNCTION; Schema: potions; Owner: delain
+--
+
+CREATE or REPLACE FUNCTION potions.pot_force_forte(integer) RETURNS text
+LANGUAGE plpgsql
+AS $_$/*********************************************************/
+/* function pot_force_forte                              */
+/* parametres :                                          */
+/*  $1 = personnage qui boit la potion                   */
+/* Sortie :                                              */
+/*  code_retour = texte exploitable par php              */
+/*********************************************************/
+/**************************************************************/
+/* 	       			                              */
+/**************************************************************/
+declare
+  personnage alias for $1;	-- perso_cod
+  code_retour text;				-- code retour
+  v_gobj_cod integer;			-- code de l'objet générique
+  duree integer;	-- Duree de l'effet
+  effet integer;	-- Force de l'effet
+
+
+
+begin
+  /*********************************************************/
+  /*        I N I T I A L I S A T I O N S                  */
+  /*********************************************************/
+  v_gobj_cod := 525;
+  code_retour := '';
+  /*Partie générique pour toutes les potions*/
+  select into code_retour potions.potion_generique(personnage,v_gobj_cod);
+  if not found then
+    code_retour := code_retour||'Erreur ! Fonction générique non trouvée ';
+  elsif substring(code_retour,1,1) != '0' then
+    return split_part(code_retour,';',2);
+  /*Tous les controles sont OK, on passe alors aux effets de la potion uniquement*/
+  else
+    code_retour := split_part(code_retour,';',2);
+    duree := lancer_des(1,10) + 25;
+    effet := lancer_des(2,4) + 2;
+    perform f_modif_carac(personnage,'FOR',duree,effet);
+    code_retour := code_retour||'<br>Vous êtes énervé, très énervé, le tissus de vos vêtements commence à se tendre, de la sueur perle sur votre front, la couleur de votre peau ... change !
+<br>Vous devenez tout vert !
+<br><br>C''est bon j''rigole, mais vous êtes sacrément plus fort maintenant !';
+  end if;
+  return code_retour;
+end;	$_$;
+
+
+ALTER FUNCTION potions.pot_force_forte(integer) OWNER TO delain;
