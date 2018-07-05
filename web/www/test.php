0321c62b850194b0e21a2c9eab0ecb541f27f3df
Marlyza <maria.lyzzie@gmail.com>
2018-07-05 11:09:29 +0200

    page determiner taille ecran

---
+++ web/www/test.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Les souterrains de Delain - Page de test définition écran</title>
+
+</head>
+<body>
+
+<script type="text/javascript">
+if (document.body)
+{
+    var larg = (document.body.clientWidth);
+    var haut = (document.body.clientHeight);
+}
+else
+{
+    var larg = (window.innerWidth);
+    var haut = (window.innerHeight);
+}
+document.write("Taille de l'écran: " + larg + "x "+haut+" pixels");
+
+</script>
+
+</body>
+</html>
