4067f57d250476944a770c4f7c576fa9499e9f75
Marlyza <maria.lyzzie@gmail.com>
2018-09-11 20:16:58 +0200

    Nouvelles potions

--- sql/20180911_0001_fonction_potion.sql
+++ sql/20180911_0001_fonction_potion.sql
@@ -3,4 +3,8 @@
 insert into potions.fonction_potion (fpot_gobj_cod, fpot_fonction, fpot_niveau) values 
 (1265, 'pot_serum', 4),
 (1266, 'pot_remede', 4),
-(1267, 'pot_antidote', 4);
+(1267, 'pot_antidote', 4),
+(1286, 'pot_biafine', 4),
+(1287, 'pot_deadbull', 4),
+(1288, 'pot_prosa_cola', 4),
+(1289, 'pot_lithium', 4);
