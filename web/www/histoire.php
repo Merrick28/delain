7cc3676175d6ebdf784bdb686cafd510d5e84593
Merrick28 <stephane.dewitte@gmail.com>
2018-12-15 13:58:08 +0100

    Sonarqube

--- web/www/histoire.php
+++ web/www/histoire.php
@@ -1,47 +1,83 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title></title>
-<link rel="stylesheet" type="text/css" href="style.php">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.php">
 </head>
 <body>
 <?php include 'jeu/tableau.php';
-Titre('L\'histoire')?>
-<div class="barrLbord"><div class="barrRbord">
-<p class="texteNorm">
-<br>Delain, cité magnifique, reconnue dans tout le Royaume d'Azgaran pour ses routes pavées d'or, ses monuments à la gloire des Dieux, ses fontaines d'où jaillit l'eau la plus pure, ses habitants, modèles de courtoisie, ses philosophes toujours en quête de Savoir, ses magiciens toujours au fait de leur art
-<br><br>Enfin, tout ca c'est bien beau, mais ce n'est plus vrai, car un jour, disons que ca c'est passé il y a 1473 ans pour faire simple, mais pour être précis, on pourra dire aussi que c'était un dimanche après midi très ensoleillé sur la place de Festivités Joyeuses.
-<br>Donc, en ce bel après midi, les démonologues apprentis étaient réunis, assis en cercle autour de la fontaine d'Or.
-<br>Si quelqu'un avait été présent et toujours en vie pour nous raconter ce qui s'y est passé, voici ce que nous aurions entendu :
-<br><br>- Moi je vous dis que Astaroth, c'est le plus puissant des démons !
-<br>- Tu dis n'importe quoi, Glaviot, Astaroth il arrive pas à la cheville de Toumimi, lui, c'est le plus fort.
-<br>- Mais non, le plus puissant des démons, c'est Trèpuyssan. Lui il crache des flammes, il viole les femmes et il fait tomber la pluie, s'écria une jeune homme au cheveux roux.
-<br>- Non, non, non, Molard, tu dis n'importe quoi, et vous autres aussi d'ailleurs. Et puis, j'ai un moyen très simple de savoir lequel est le plus puissant.
-<br>- Ah ? s'étonna Molard. Et c'est quoi ce moyen ?
-<br>- Et bien, il suffit d'en invoquer un et de lui demander
-<br><br>Tous les démonologues furent consternés par cette annonce de leur condisciple
-<br><br>- Mais tu n'y penses pas ! L'invocation de démons majeurs n'est pas encore au programme de la 3è année d'étude ! On ne l'apprend qu'à la fin de la 32è année ! fit Klikli, le jeune roux de tout à l'heure
-<br>- Mais c'est pas grave, j'ai espionné Maitre Tralek, j'ai vu comment il a fait, pis je lui ai chourré son livre d'invocation, l'<em>Invocatum Grandum Majestum Demonicus</em>
-<br>- Comment ? Mais tu n'as pas le droit !
-<br>- Je sais, mais j'ai pris le gauche, et vu que je suis ambidextre, ca gene pas, dit-il à ses condisciples étonnés par tant de désinvolture. D'ailleurs, je l'ai ramené ici. Je l'ai lu, et à mon avis, faut pas être très futé pour y arriver
-<br><br>Ayant réussi à convaincre ses camarades, Gors (puisque c'était son nom) commença l'invocation et...il faut croire qu'il n'était vraiment pas très futé, car, bien évidemment, l'invocation a raté
-<br>Des nuages noirs s'amoncelèrent au dessus de la magnifique ville de Delain, une tempête telle que personne n'en avait jamais vu  détruisit la cité en moins de temps qu'il n'en faut pour changer ses sandales
-<br>Le bon point dans cette histoires, c'est que les habitants n'eurent pas le temps de comprendre ce qui leur arrivait, à un moment il y avait la plus belle ville du royaume, et le moment d'après, il ne restait qu'un monceau de bâtiments en ruine agrémentés ca et et là par des cadavres à divers état de putréfaction, et, au milieu de tout ca, Gors était debout et contemplait la ville anéantie.
-<br>Mais quelque chose avait changé en lui. Déjà, ses yeux marrons s'étaient teintés de rouge sang, puis ses cheveux avaient disparu pour laisser place à une chevelure de feu ardent. Mais le détail le plus important c'est qu'il avait changé de carrure pour passer du démonologue apprenti malingre de base pour devenir un... machin tout en muscle, la peau rouge et avec une voix très forte.
-<br><br>A ce stade de l'histoire, on peut préciser que l'invocation n'avait pas totalement raté, disons plutôt qu'elle ne s'était pas passée comme prévu.
-<br>Un démon avait bien été invoqué, c'était d'ailleurs le démon le plus puissant de la création, mais le problème, c'est qu'il avait pris possession de son invocateur pour accomplir ses noirs desseins.
-<br><br>Le démon sans nom (car les démons mineurs avaient tellement peur de lui qu'ils ne prononçaient jamais son nom et qu'il était tombé dans l'oubli) commença à ravager le pays.
-<br>En l'espace d'une journée, des milliers d'hommes et femmes étaient passés de vie à trépas, des centaines de villes étaient retournées à l'état de carrière de pierre et la nature avait fait un gigantesque bond en arrière dans l'histoire de l'évolution.
-<br><br>Tous les habitants firent front commun pour combattre le démon qu'ils avaient fini par nommer Malkiar le Rouge (Malkiar veut dire Engeance démonique putride et puante en vieil Elfique)
-<br>Le combat fut rude et dura des mois, de toutes les races qui avaient décidé de combattre le démon au début, il n'en restait que trois : les Humains, les Elfes et les Nains.
-<br>Dans un dernier effort, ils avaient réussi à repousser Malkiar jusqu'au ruines de l'ancienne Delain. Les plus grands prêtres des trois races unirent leurs forces pour conjurer le démon.
-<br>Malkiar fut donc vaincu, enfin, plutôt repoussé dans les ténèbres du labyrinthe que ses démons mineurs avaient creusé sous les ruines de Delain.
-<br>Les prêtres placèrent un sceau sur l'entrée du labyrinthe et interdirent la pratique de la démonologie.
-<br><br><br>Aujourd'hui, le temps a passé, 1473 années se sont écoulées depuis cette sinistre victoire. L'Histoire a maintenant oublié les évènements qui se sont produits.
-<br><br>Il est dit que le sceau a perdu son pouvoir, il est dit que Malkiar s'est réveillé et qu'il récupère ses forces par l'entremise des monstres qu'il a invoqué dans le labyrinthe, il est dit que des personnes courageuses se sont aventurées dans les méandres obscures des sous terrains de Delain
-<br>Mais jusqu'à maintenant, personne n'en est encore ressorti pour le raconter
-</p>
-</div></div>
-<?php Bordure_Tab()?>
+Titre('L\'histoire') ?>
+<div class="barrLbord">
+    <div class="barrRbord">
+        <p class="texteNorm">
+            <br>Delain, cité magnifique, reconnue dans tout le Royaume d'Azgaran pour ses routes pavées d'or, ses
+            monuments à la gloire des Dieux, ses fontaines d'où jaillit l'eau la plus pure, ses habitants, modèles de
+            courtoisie, ses philosophes toujours en quête de Savoir, ses magiciens toujours au fait de leur art
+            <br><br>Enfin, tout ca c'est bien beau, mais ce n'est plus vrai, car un jour, disons que ca c'est passé il y
+            a 1473 ans pour faire simple, mais pour être précis, on pourra dire aussi que c'était un dimanche après midi
+            très ensoleillé sur la place de Festivités Joyeuses.
+            <br>Donc, en ce bel après midi, les démonologues apprentis étaient réunis, assis en cercle autour de la
+            fontaine d'Or.
+            <br>Si quelqu'un avait été présent et toujours en vie pour nous raconter ce qui s'y est passé, voici ce que
+            nous aurions entendu :
+            <br><br>- Moi je vous dis que Astaroth, c'est le plus puissant des démons !
+            <br>- Tu dis n'importe quoi, Glaviot, Astaroth il arrive pas à la cheville de Toumimi, lui, c'est le plus
+            fort.
+            <br>- Mais non, le plus puissant des démons, c'est Trèpuyssan. Lui il crache des flammes, il viole les
+            femmes et il fait tomber la pluie, s'écria une jeune homme au cheveux roux.
+            <br>- Non, non, non, Molard, tu dis n'importe quoi, et vous autres aussi d'ailleurs. Et puis, j'ai un moyen
+            très simple de savoir lequel est le plus puissant.
+            <br>- Ah ? s'étonna Molard. Et c'est quoi ce moyen ?
+            <br>- Et bien, il suffit d'en invoquer un et de lui demander
+            <br><br>Tous les démonologues furent consternés par cette annonce de leur condisciple
+            <br><br>- Mais tu n'y penses pas ! L'invocation de démons majeurs n'est pas encore au programme de la 3è
+            année d'étude ! On ne l'apprend qu'à la fin de la 32è année ! fit Klikli, le jeune roux de tout à l'heure
+            <br>- Mais c'est pas grave, j'ai espionné Maitre Tralek, j'ai vu comment il a fait, pis je lui ai chourré
+            son livre d'invocation, l'<em>Invocatum Grandum Majestum Demonicus</em>
+            <br>- Comment ? Mais tu n'as pas le droit !
+            <br>- Je sais, mais j'ai pris le gauche, et vu que je suis ambidextre, ca gene pas, dit-il à ses
+            condisciples étonnés par tant de désinvolture. D'ailleurs, je l'ai ramené ici. Je l'ai lu, et à mon avis,
+            faut pas être très futé pour y arriver
+            <br><br>Ayant réussi à convaincre ses camarades, Gors (puisque c'était son nom) commença l'invocation
+            et...il faut croire qu'il n'était vraiment pas très futé, car, bien évidemment, l'invocation a raté
+            <br>Des nuages noirs s'amoncelèrent au dessus de la magnifique ville de Delain, une tempête telle que
+            personne n'en avait jamais vu détruisit la cité en moins de temps qu'il n'en faut pour changer ses sandales
+            <br>Le bon point dans cette histoires, c'est que les habitants n'eurent pas le temps de comprendre ce qui
+            leur arrivait, à un moment il y avait la plus belle ville du royaume, et le moment d'après, il ne restait
+            qu'un monceau de bâtiments en ruine agrémentés ca et et là par des cadavres à divers état de putréfaction,
+            et, au milieu de tout ca, Gors était debout et contemplait la ville anéantie.
+            <br>Mais quelque chose avait changé en lui. Déjà, ses yeux marrons s'étaient teintés de rouge sang, puis ses
+            cheveux avaient disparu pour laisser place à une chevelure de feu ardent. Mais le détail le plus important
+            c'est qu'il avait changé de carrure pour passer du démonologue apprenti malingre de base pour devenir un...
+            machin tout en muscle, la peau rouge et avec une voix très forte.
+            <br><br>A ce stade de l'histoire, on peut préciser que l'invocation n'avait pas totalement raté, disons
+            plutôt qu'elle ne s'était pas passée comme prévu.
+            <br>Un démon avait bien été invoqué, c'était d'ailleurs le démon le plus puissant de la création, mais le
+            problème, c'est qu'il avait pris possession de son invocateur pour accomplir ses noirs desseins.
+            <br><br>Le démon sans nom (car les démons mineurs avaient tellement peur de lui qu'ils ne prononçaient
+            jamais son nom et qu'il était tombé dans l'oubli) commença à ravager le pays.
+            <br>En l'espace d'une journée, des milliers d'hommes et femmes étaient passés de vie à trépas, des centaines
+            de villes étaient retournées à l'état de carrière de pierre et la nature avait fait un gigantesque bond en
+            arrière dans l'histoire de l'évolution.
+            <br><br>Tous les habitants firent front commun pour combattre le démon qu'ils avaient fini par nommer
+            Malkiar le Rouge (Malkiar veut dire Engeance démonique putride et puante en vieil Elfique)
+            <br>Le combat fut rude et dura des mois, de toutes les races qui avaient décidé de combattre le démon au
+            début, il n'en restait que trois : les Humains, les Elfes et les Nains.
+            <br>Dans un dernier effort, ils avaient réussi à repousser Malkiar jusqu'au ruines de l'ancienne Delain. Les
+            plus grands prêtres des trois races unirent leurs forces pour conjurer le démon.
+            <br>Malkiar fut donc vaincu, enfin, plutôt repoussé dans les ténèbres du labyrinthe que ses démons mineurs
+            avaient creusé sous les ruines de Delain.
+            <br>Les prêtres placèrent un sceau sur l'entrée du labyrinthe et interdirent la pratique de la démonologie.
+            <br><br><br>Aujourd'hui, le temps a passé, 1473 années se sont écoulées depuis cette sinistre victoire.
+            L'Histoire a maintenant oublié les évènements qui se sont produits.
+            <br><br>Il est dit que le sceau a perdu son pouvoir, il est dit que Malkiar s'est réveillé et qu'il récupère
+            ses forces par l'entremise des monstres qu'il a invoqué dans le labyrinthe, il est dit que des personnes
+            courageuses se sont aventurées dans les méandres obscures des sous terrains de Delain
+            <br>Mais jusqu'à maintenant, personne n'en est encore ressorti pour le raconter
+        </p>
+    </div>
+</div>
+<?php Bordure_Tab() ?>
 </div>
 </body>
 </html>
--- web/www/inter.php
+++ web/www/inter.php
@@ -1,7 +1,11 @@
-<html> 
-  <body> 
-  <SCRIPT LANGUAGE="JavaScript"> 
-  top.location.href = 'index.php';
-  </SCRIPT> 
-  </body> 
-  </html> 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Retour</title>
+</head>
+<body>
+<SCRIPT LANGUAGE="JavaScript">
+    top.location.href = 'index.php';
+</SCRIPT>
+</body>
+</html>
