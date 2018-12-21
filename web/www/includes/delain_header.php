365a2fd018fad087678e0feca11487356a086341
Marlyza <maria.lyzzie@gmail.com>
2018-12-21 16:17:03 +0100

    securisation register_globals 3

--- web/www/includes/delain_header.php
+++ web/www/includes/delain_header.php
@@ -40,7 +40,8 @@ function writelog($textline,$filename='undefined',$verbose=true)
 /**
  * Récupérer la véritable adresse IP d'un visiteur
  */
-function get_ip() {
+function get_ip()
+{
     // IP si internet partagé
     if (isset($_SERVER['HTTP_CLIENT_IP'])) {
         return $_SERVER['HTTP_CLIENT_IP'];
@@ -55,6 +56,21 @@ function get_ip() {
     }
 }
 
+// On recherche des chaines qui pourraient faire penser à de l'injection SQL comme "SELECT * FROM", "DELETE FROM" et "UPDATE FROM"
+// mais il faut penser qu'un paramètre du type "&action=delete" est valide
+// =>  Pour commencer on  va interdire tout ce qui contient "FROM" et une autre chaine du type "SELECT", "DELETE" ou "UPDATE"
+function is_hacking_string($value)
+{
+    if (stripos($value, "from" ) !== false)
+    {
+        if ((stripos($value, "select" ) !== false)||(stripos($value, "delete" ) !== false)||(stripos($value, "update" ) !== false))
+        {
+             return $value ;
+        }
+    }
+    return "";
+}
+
 function register_globals($order = 'egpcs')
 {
     // define a subroutine
@@ -62,25 +78,27 @@ function register_globals($order = 'egpcs')
     {
         function register_global_array(array $superglobal)
         {
-            $detection_hacking = false ;    // par défaut tout va bien !!
+            $hacking_value = "" ;           // Chaine qui est détéectée comme du hacking
             foreach ($superglobal as $varname => $value)
             {
-                // On recherche des chaines qui pourraient faire penser à de l'injection SQL comme "SELECT * FROM", "DELETE FROM" et "UPDATE FROM"
-                // mais il faut penser qu'un paramètre du type "&action=delete" est valide
-                // =>  Pour commencer on  va interdire tout ce qui contient "FROM" et une autre chaine du type "SELECT", "DELETE" ou "UPDATE"
-                if (!is_array($value) && (stripos($value, "from" ) !== false))
+                if (is_array($value))
                 {
-
-                    if ((stripos($value, "select" ) !== false)||(stripos($value, "delete" ) !== false)||(stripos($value, "update" ) !== false))
+                    foreach ($value as $v)
                     {
-                        $detection_hacking = true ;
+                        $hacking_value = is_hacking_string($v);
+                        if ($hacking_value != '') break;
                     }
                 }
+                else
+                {
+                    $hacking_value = is_hacking_string($value);
+                }
+
                 // C'est louche, on ne permet pas d'aller plus loin!!
-                if ($detection_hacking)
+                if ($hacking_value != '')
                 {
 
-                    $log = "Tentative de ".get_ip()." sur la page ".$_SERVER["REQUEST_URI"]."\nInjection sur le paramètre '$varname' : $value\n ";
+                    $log = "Tentative de ".get_ip()." sur la page ".$_SERVER["REQUEST_URI"]."\nInjection sur le paramètre '{$varname}' : {$hacking_value}\n ";
                     writelog($log, 'hacking',false);
                     die('<br>Une erreur est survenue, si le problème se répète, merci de contacter les administrateurs sur le <a target="_blank" href="https://forum.jdr-delain.net/viewforum.php?f=2&sid=9a837e88f0b38247280c5869a6a6a99c">Forum bug</a><br><br>Pour faciliter le débuggage veuillez préciser la date et l\'heure de l\'incident: <b>'.date("Y-m-d H:i:s").'</b>');
                 }
