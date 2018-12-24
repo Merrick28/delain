beddbf5597f6f6408a99a995c7d40d5fed16c885
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2018-12-24 11:31:13 +0100

    Bugfix etat de santé dans combat.php

--- shell/livraisons.sh
+++ shell/livraisons.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 source `dirname $0`/env
-echo "===========================" >> `dirname $0`/livraison.log
-echo "= LIVRAISONS SQL EN COURS =" >> `dirname $0`/livraison.log
+echo "===========================" >> ${shellroot}/livraison.log
+echo "= LIVRAISONS SQL EN COURS =" >> ${shellroot}/livraison.log
 for f in `find $livroot -type f| grep -v "initial_import.sql"|sort`; do
   livexist=`$shellroot/livraison_exists.sh $(basename $f)`
   if [ "$livexist" -eq "0" ];then
       echo "LIVRAISONS A TRAITER : $f" >> /dev/null 2>&1
-      $psql -A -q -t -d delain -U webdelain -f $f >> `dirname $0`/livraison.log 2>&1
-      $psql -A -q -t -d delain -U webdelain << EOF >> `dirname $0`/livraison.log 2>&1
+      $psql -A -q -t -d delain -U webdelain -f $f >> ${shellroot}/livraison.log 2>&1
+      $psql -A -q -t -d delain -U webdelain << EOF >> ${shellroot}/livraison.log 2>&1
 insert into livraisons (liv_fichier) values ('$(basename $f)');
 EOF
   else
-    echo "$f déjà traité" >> `dirname $0`/livraison.log 2>&1
+    echo "$f déjà traité" >> ${shellroot}/livraison.log 2>&1
   fi
 done
 # livraison des fonctions
 for f in `find $livfunc -type f| sort`; do
-      echo "LIVRAISONS A TRAITER : $f" >> /dev/null 2>>`dirname $0`/livraison.log
-      $psql -A -q -t -d delain -U webdelain -f $f >> `dirname $0`/livraison.log 2>&1
+      echo "LIVRAISONS A TRAITER : $f" >> /dev/null 2>>${shellroot}/livraison.log
+      $psql -A -q -t -d delain -U webdelain -f $f >> ${shellroot}/livraison.log 2>&1
 done
