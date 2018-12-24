2bcd4692a75f36cf4f4389edb5a9192e57309c3e
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2018-12-24 11:35:36 +0100

    Bugfix etat de santé dans combat.php

--- shell/livraisons.sh
+++ shell/livraisons.sh
@@ -5,17 +5,14 @@ echo "= LIVRAISONS SQL EN COURS =" >> ${shellroot}/livraison.log
 for f in `find $livroot -type f| grep -v "initial_import.sql"|sort`; do
   livexist=`$shellroot/livraison_exists.sh $(basename $f)`
   if [ "$livexist" -eq "0" ];then
-      echo "LIVRAISONS A TRAITER : $f" >> /dev/null 2>&1
+      echo "LIVRAISONS A TRAITER : $f" >> ${shellroot}/livraison.log
       $psql -A -q -t -d delain -U webdelain -f $f >> ${shellroot}/livraison.log 2>&1
       $psql -A -q -t -d delain -U webdelain << EOF >> ${shellroot}/livraison.log 2>&1
 insert into livraisons (liv_fichier) values ('$(basename $f)');
 EOF
-  else
-    echo "$f déjà traité" >> ${shellroot}/livraison.log 2>&1
-  fi
 done
 # livraison des fonctions
+echo "= LIVRAISONS DES FONCTIONS =" >> ${shellroot}/livraison.log
 for f in `find $livfunc -type f| sort`; do
-      echo "LIVRAISONS A TRAITER : $f" >> /dev/null 2>>${shellroot}/livraison.log
       $psql -A -q -t -d delain -U webdelain -f $f >> ${shellroot}/livraison.log 2>&1
 done
