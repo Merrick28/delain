2545ebd13efe36f32af0762ed223cf26943a94fb
Merrick28 <stephane.dewitte@gmail.com>
2019-01-03 20:20:08 +0100

    Bugfix don brouzoufs

--- web/www/jeu_test/action.php
+++ web/www/jeu_test/action.php
@@ -791,7 +791,7 @@ if (!$compte->is_admin() || ($compte->is_admin_monstre() && $perso->perso_type_p
             $contenu_page .= $perso->change_grade($_REQUEST['dieu']);
             break;
         case 'don_br':
-            $contenu_page .= $perso->change_grade($_REQUEST['dest'], $_REQUEST['qte']);
+            $contenu_page .= $perso->don_br($_REQUEST['dest'], $_REQUEST['qte']);
             break;
         case 'vente_auberge':
             $contenu_page .= $perso->vente_auberge($_REQUEST['objet']);
