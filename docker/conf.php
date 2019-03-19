559704c13142bff2571d07894157500363f2bb8d
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2019-03-19 13:34:51 +0100

    test sur shell

---
+++ docker/conf.php
@@ -0,0 +1,22 @@
+<?php
+
+define('G_URL','localhost/delain/'); // soit finir par un /
+define('G_CHE',__DIR__ . '/../'); // NE PAS TOUCHER
+define('G_IMAGES','http://localhost/images/');
+define('IMG_PATH',G_IMAGES);
+
+// connexion base de données
+define('SERVER_PROD',false); // if true, we'll use a service
+define('SERVER_HOST','postgres');
+define('SERVER_USERNAME','delain');
+define('SERVER_PASSWORD','mypassword');
+define('SERVER_DBNAME','delain');
+
+// URL API
+define('URL_API','http://localhost/api/');
+
+// SMTP
+define('SMTP_HOST','smtp.free.fr');    // The smtp server host/ip
+define('SMTP_PORT',25);
+define('STMP_USER','');
+define('STMP_PASSWORD','');
--- shell/livraisons.sh
+++ shell/livraisons.sh
@@ -6,8 +6,8 @@ for f in `find $livroot -type f| grep -v "initial_import.sql"|sort`; do
   livexist=`$shellroot/livraison_exists.sh $(basename $f)`
   if [ "$livexist" -eq "0" ];then
       echo "LIVRAISONS A TRAITER : $f" >> ${shellroot}/livraison.log
-      $psql -A -q -t -d delain -U webdelain -f $f >> ${shellroot}/livraison.log 2>&1
-      $psql -A -q -t -d delain -U webdelain << EOF >> ${shellroot}/livraison.log 2>&1
+      $psql -A -q -t -d delain -U ${USERNAME} -f $f >> ${shellroot}/livraison.log 2>&1
+      $psql -A -q -t -d delain -U ${USERNAME} << EOF >> ${shellroot}/livraison.log 2>&1
 insert into livraisons (liv_fichier) values ('$(basename $f)');
 EOF
 fi
@@ -15,5 +15,5 @@ done
 # livraison des fonctions
 echo "= LIVRAISONS DES FONCTIONS =" >> ${shellroot}/livraison.log
 for f in `find $livfunc -type f| sort`; do
-      $psql -A -q -t -d delain -U webdelain -f $f >> ${shellroot}/livraison.log 2>&1
+      $psql -A -q -t -d delain -U ${USERNAME} -f $f >> ${shellroot}/livraison.log 2>&1
 done
