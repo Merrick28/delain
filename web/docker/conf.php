b653835770f1ff696f70be5ce309acef10912525
Merrick28 <stephane.dewitte@gmail.com>
2019-04-10 11:30:53 +0200

    Manque SSL

--- web/docker/conf.php
+++ web/docker/conf.php
@@ -1,13 +1,13 @@
 <?php
 
-define('G_URL','localhost/delain/'); // soit finir par un /
+define('G_URL','localhost:8080/'); // soit finir par un /
 define('G_CHE',__DIR__ . '/../'); // NE PAS TOUCHER
-define('G_IMAGES','http://localhost/images/');
+define('G_IMAGES','http://localhost:8080/images/');
 define('IMG_PATH',G_IMAGES);
 
 // connexion base de données
 define('SERVER_PROD',false); // if true, we'll use a service
-define('SERVER_HOST','localhost');
+define('SERVER_HOST','postgres');
 define('SERVER_USERNAME','delain');
 define('SERVER_PASSWORD','mypassword');
 define('SERVER_DBNAME','delain');
@@ -20,3 +20,5 @@ define('SMTP_HOST','smtp.free.fr');    // The smtp server host/ip
 define('SMTP_PORT',25);
 define('STMP_USER','');
 define('STMP_PASSWORD','');
+
+define('DOCKER',true);
