1d4b64454e6d7ed5af7261b87d6b436e0537122f
Marlyza <maria.lyzzie@gmail.com>
2019-05-12 19:36:55 +0200

    type objet: bottes/gants

---
+++ sql/20190512_0001_ajout_type_objets.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.type_objet
+  (tobj_libelle, tobj_ident_comp_cod, tobj_max_equip, tobj_identifie_auto, tobj_equipable, tobj_affichage_char)
+VALUES
+  ('Gants', 3, 1, 0, 1, ']'),
+  ('Bottes', 3, 1, 0, 1, ']');
