bd68632bd4fe84110275988b13cd8afcd390e7d4
Marlyza <maria.lyzzie@gmail.com>
2019-07-09 20:19:28 +0200

    Ajout race

--- fonctions_sql/cron_dissip_objet.sql
+++ fonctions_sql/cron_dissip_objet.sql
@@ -19,14 +19,15 @@ begin
 -- recuperation du coefficient d'absorbtion des vents.
 select into v_coeff to_number( getparm_t(142), '9999999.99');
 
-
+/*
+-- mettre en pause le temps de faire un état des lieux!
 for obj in select obj_cod, pobj_pos_cod, obj_poids from objets
   join objet_position on pobj_obj_cod=obj_cod
   join objet_generique on gobj_cod=obj_gobj_cod
   join type_objet on tobj_cod=gobj_tobj_cod
   where pobj_dlache<now() - (tobj_degradation::text || ' DAYS')::interval AND tobj_degradation>0
   order by pobj_dlache
-  limit 1
+  limit 1000
 
   loop
 
@@ -45,6 +46,6 @@ for obj in select obj_cod, pobj_pos_cod, obj_poids from objets
 
 
   end loop;
-
+*/
 end;$_$;
 ALTER FUNCTION public.cron_dissip_objet()  OWNER TO delain;
---
+++ sql/20190709_00001_nouvelle_race.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.race(race_nom, race_description) VALUES
+     ('Ange déchu', 'C''est un ange exilé, banni en punition de sa rébellion. Craignez son courrou!');
+
+
