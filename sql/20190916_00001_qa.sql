a7cf7b8d10edd7cddbc09ba50d88c63359441aa5
Marlyza <maria.lyzzie@gmail.com>
2019-09-23 21:48:25 +0200

    QA: modification quete dialogue avec ajout d'un PNJ

--- sql/20190916_00001_qa.sql
+++ sql/20190916_00001_qa.sql
@@ -1,7 +1,7 @@
 UPDATE quetes.aquete_etape_modele
 SET
 aqetapmodel_modele='Voici ce que [2] vous propose comme services:',
-aqetapmodel_parametres='[1:delai|1%1],[2:perso|1%1],[3:valeur|1%1],[4:valeur|1%1],[5:valeur|1%1],[6:echange|0%0]',
+aqetapmodel_parametres='[1:delai|1%1],[2:perso|1%0],[3:valeur|1%1],[4:valeur|1%1],[5:valeur|1%1],[6:echange|0%0]',
 aqetapmodel_param_desc='Délai alloué pour cette étape.|Le joueur doit se trouver sur la case de ce PNJ pour faire l''échange.|C''est le nombre d''échange maximum qui pourront-être choisis par le joueur (0 pour illimité)|C''est le nombre maximum autorisé pour chaque échange (0 pour illimité)|C''est le nombre maximum d''objets autorisés tout échanges confondues (0 pour illimité)|Liste des transactions: les objets à acquerir pour le joueur contre les objets qui servent de monnaie d''échange.<br><u><b>NOTA</b></u>: Si partie gauche (objet à donner) est laissée à 0 (0x0), la partie droite sert alors de cout additionnel à l''objet situé au-dessus.'
 WHERE aqetapmodel_tag = '#ECHANGE #OBJET';
 
