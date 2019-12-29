54f6b6d89d98c4daaf8cccad35bc0a76910309d1
Merrick28 <stephane.dewitte@gmail.com>
2019-12-29 16:40:43 +0100

    tests sur TU et docker

--- web/tests/wait.sh
+++ web/tests/wait.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 until PGPASSWORD=mypassword psql -h "delain_dbtu" -U "delain" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep 10
 done
 echo "Postgres UP !"
