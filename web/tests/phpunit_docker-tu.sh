aa22395c5c0ec88e083d1b45fa609b1234737f12
Merrick28 <stephane.dewitte@gmail.com>
2019-12-29 16:04:24 +0100

    tests sur TU et docker

--- web/tests/phpunit_docker-tu.sh
+++ web/tests/phpunit_docker-tu.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # on attend la connection à postgres
-while ! nc -z delain_dbtu 5432; do
-  sleep 10
-  echo "Waiting for postgres to be up"
-done
 docker exec webtu /home/delain/delain/web/vendor/bin/phpunit /home/delain/delain/web/tests/
