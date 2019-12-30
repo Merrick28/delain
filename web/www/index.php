e3e800f9d082b298e1fff98fd2a3acb55a31f94c
Merrick28 <stephane.dewitte@gmail.com>
2019-12-30 09:20:40 +0100

    [ci skip] bugfix news

--- web/www/index.php
+++ web/www/index.php
@@ -2,14 +2,7 @@
 $nouvelle_version = 1;
 
 
-// déjà, on rejette les false news id
-/*if(isset($_REQUEST['start_news']))
-{
-    if(!is_int($_REQUEST['start_news']))
-    {
-        die('test');
-    }
-}*/
+//$toolbar->addDataCollector(new \Fabfuel\Prophiler\DataCollector\PDO());
 
 // par défaut, on n'est pas authentifié
 $verif_auth = false;
@@ -22,7 +15,7 @@ $param = new parametres;
 $gmon = new monstre_generique;
 $gmon->getRandom();
 
-
+// chargement des news
 $news       = new news;
 $numberNews = $news->getNumber();
 
@@ -34,51 +27,35 @@ if (!isset($_REQUEST['start_news']))
 }
 else
 {
-    $start_news = (int)$_REQUEST['start_news'];
+    $start_news = $_REQUEST['start_news'];
 }
 if ($start_news < 0)
 {
     $start_news = 0;
 }
-/*
-// si tentative de hack, on affiche la page par défaut
-// ça évite les logs, et ça permet d'afficher quand
-// même de la pub :-)
-if ( (int) $start_news !== $start_news )
-{
-    $start_news = 0;
-}
-*/
+
 $tabNews = $news->getNews($start_news);
 
-/*$callapi = new callapi();
-if ($callapi->call(API_URL . '/news?start_news=' . $start_news,
-                   'GET'))
-{
-    $tabNews = json_decode($callapi->content);
-} else
-{
-    die('Erreur sur appel API ' . $callapi->content);
-}*/
 
 require_once CHEMIN . 'choix_pub.php';
 $pub = choix_pub_index();
 
-/** @var Twig_Loader_Filesystem $twig */
-$template     = $twig->load('index.twig');
+
+
+$template = $twig->load('index.twig');
 $options_twig = array(
     'PERCENT_FINANCES'  => $percent_finances,
     'AVENTURIERS_MORTS' => $param->getparm(64),
     'MONSTRES_MORTS'    => $param->getparm(65),
     'FAMILIERS_MORTS'   => $param->getparm(66),
     'GMON'              => $gmon,
-    'TABNEWS'           => $tabNews->news,
+    'TABNEWS'           => $tabNews,
     'START_NEWS'        => $start_news,
-    'NB_NEWS'           => $tabNews->numberNews,
+    'NB_NEWS'           => $numberNews,
     'PUB'               => $pub,
 
 
 );
-echo $template->render(array_merge($options_twig_defaut, $options_twig));
+echo $template->render(array_merge($options_twig_defaut,$options_twig));
 
 
