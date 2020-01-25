1b29e9b95e75fdf0898a2df65a59c2d3766cac10
Merrick28 <stephane.dewitte@gmail.com>
2020-01-25 15:18:20 +0100

    bugfix visu desc objet

--- web/www/jeu_test/valide_decalage_dlt.php
+++ web/www/jeu_test/valide_decalage_dlt.php
@@ -2,21 +2,27 @@
 include "blocks/_header_page_jeu.php";
 ob_start();
 $erreur = 0;
-if (!isset($temps_dlt)) {
+if (!isset($temps_dlt))
+{
     echo("<p>Vous devez saisir une valeur de temps !!");
     $erreur = 1;
 }
-if ($erreur == 0) {
-    if ($temps_dlt <= 0) {
+if ($erreur == 0)
+{
+    if ($temps_dlt <= 0)
+    {
         echo("<p>Vous ne pouvez pas saisir une valeur nulle ou négative !");
         $erreur = 1;
     }
 }
-if ($erreur == 0) {
-    if (!isset($methode)) {
+if ($erreur == 0)
+{
+    if (!isset($methode))
+    {
         $methode = "debut";
     }
-    switch ($methode) {
+    switch ($methode)
+    {
         case "debut":
             $req = "select to_char((perso_dlt + '$temps_dlt minutes'::interval),'DD/MM/YYYY à hh24:mi:ss') as nvdlt,
                                to_char(prochaine_dlt($perso_cod) + '$temps_dlt minutes'::interval,'DD/MM/YYYY à hh24:mi:ss') as nxtdlt
@@ -24,7 +30,7 @@ if ($erreur == 0) {
 					where perso_cod = $perso_cod ";
             $db->query($req);
             $db->next_record();
-            $nvdlt = $db->f('nvdlt');
+            $nvdlt  = $db->f('nvdlt');
             $nxtdlt = $db->f('nxtdlt');
             ?>
             Etes vous sûr de vouloir décaler votre dlt de <?php echo $temps_dlt ?> minutes ? <br/>
@@ -37,7 +43,8 @@ if ($erreur == 0) {
             break;
         case "validation":
             $temps_dlt = round($temps_dlt);
-            $req = "update perso set perso_dlt = perso_dlt + '$temps_dlt minutes'::interval where perso_cod = $perso_cod ";
+            $req       =
+                "update perso set perso_dlt = perso_dlt + '$temps_dlt minutes'::interval where perso_cod = $perso_cod ";
             $db->query($req);
 
             echo("<p>Votre DLT a bien été repoussée de $temps_dlt minutes. ");
--- web/www/jeu_test/visu_desc_objet2.php
+++ web/www/jeu_test/visu_desc_objet2.php
@@ -2,12 +2,13 @@
 include "blocks/_header_page_jeu.php";
 ob_start();
 $objet = $_GET['objet'];
-if (!preg_match('/^[0-9]*$/i', $objet)) {
+if (!preg_match('/^[0-9]*$/i', $objet))
+{
     echo "<p>Anomalie sur numéro objet !";
     exit();
 }
 $autorise = 0;
-$req = "select perobj_cod from perso_objets,objets
+$req      = "select perobj_cod from perso_objets,objets
 	where perobj_perso_cod = $perso_cod
 	and perobj_obj_cod = obj_cod
 	and perobj_identifie = 'O' 
@@ -17,10 +18,11 @@ if ($db->nf() != 0)
     $autorise = 1;
 // on regarde si l'objet est dans une échoppe sur laquelle on est
 
-if ($db->is_lieu($perso_cod)) {
+if ($db->is_lieu($perso_cod))
+{
     $tab_lieu = $db->get_lieu($perso_cod);
     $lieu_cod = $tab_lieu['lieu_cod'];
-    $req = "select mstock_obj_cod from stock_magasin,objets
+    $req      = "select mstock_obj_cod from stock_magasin,objets
 		where mstock_lieu_cod = $lieu_cod
 		and mstock_obj_cod = obj_cod
 		and obj_gobj_cod = $objet";
@@ -35,7 +37,8 @@ if ($db->is_lieu($perso_cod)) {
     if ($db->nf() != 0)
         $autorise = 1;
 }
-if ($autorise == 1) {
+if ($autorise == 1)
+{
     $req = "select gobj_nom, gobj_tobj_cod, tobj_libelle, gobj_poids, gobj_pa_normal, gobj_pa_eclair, gobj_distance, gobj_deposable, gobj_comp_cod,
 				gobj_description, gobj_seuil_dex, gobj_seuil_force, gobj_niveau_min 
 			from objet_generique,type_objet 
@@ -43,23 +46,25 @@ if ($autorise == 1) {
 				and gobj_tobj_cod = tobj_cod 
 				and (gobj_visible is null or gobj_visible != 'N') ";
     $db->query($req);
-    if ($db->nf() != 0) {
+    if ($db->nf() != 0)
+    {
         $db->next_record();
-        $t_etat = 0;
-        $comp = $db->f("gobj_comp_cod");
-        $desc = $db->f("gobj_description");
-        $distance = $db->f("gobj_distance");
-        $type_objet = $db->f("gobj_tobj_cod");
+        $t_etat      = 0;
+        $comp        = $db->f("gobj_comp_cod");
+        $desc        = $db->f("gobj_description");
+        $distance    = $db->f("gobj_distance");
+        $type_objet  = $db->f("gobj_tobj_cod");
         $seuil_force = $db->f("gobj_seuil_force");
-        $seuil_dex = $db->f("gobj_seuil_dex");
-        $niveau_min = $db->f("gobj_niveau_min");
+        $seuil_dex   = $db->f("gobj_seuil_dex");
+        $niveau_min  = $db->f("gobj_niveau_min");
         echo "<p class=\"titre\">" . $db->f("gobj_nom") . "</p>";
         echo "<div class='centrer'><table>";
 
         echo "<tr>";
         echo "<td class=\"soustitre2\"><p>Type d’objet</p></td>";
         echo "<td><p>" . $db->f("tobj_libelle");
-        if ($db->f("gobj_deposable") == 'N') {
+        if ($db->f("gobj_deposable") == 'N')
+        {
             echo " <strong>non déposable !</strong>";
         }
         echo "</p></td>";
@@ -70,13 +75,15 @@ if ($autorise == 1) {
         echo "<td><p>" . $db->f("gobj_poids") . "</p></td>";
         echo "</tr>";
 
-        if ($type_objet == 1) {
+        if ($type_objet == 1)
+        {
             echo "<tr>";
             echo "<td class=\"soustitre2\"><p>Coût en PA pour une attaque normale</p></td>";
             echo "<td><p>" . $db->f("gobj_pa_normal") . "</p></td>";
             echo "</tr>";
 
-            if ($distance == 'N') {
+            if ($distance == 'N')
+            {
                 echo "<tr>";
                 echo "<td class=\"soustitre2\"><p>Coût en PA pour une attaque foudroyante</p></td>";
                 echo "<td><p>" . $db->f("gobj_pa_eclair") . "</p></td>";
@@ -103,26 +110,30 @@ if ($autorise == 1) {
             echo "<td class=\"soustitre2\"><p>Compétence utilisée</p></td>";
             echo "<td><p>" . $db->f("comp_libelle") . "</p></td>";
             echo "</tr>";
-            if ($seuil_force > 0) {
+            if ($seuil_force > 0)
+            {
                 echo "<tr>";
                 echo "<td class=\"soustitre2\"><p>Seuil de force</p></td>";
                 echo "<td><p>" . $seuil_force . "</p></td>";
                 echo "</tr>";
             }
-            if ($seuil_dex > 0) {
+            if ($seuil_dex > 0)
+            {
                 echo "<tr>";
                 echo "<td class=\"soustitre2\"><p>Seuil de dextérité</p></td>";
                 echo "<td><p>" . $seuil_dex . "</p></td>";
                 echo "</tr>";
             }
-            if ($niveau_min > 0) {
+            if ($niveau_min > 0)
+            {
                 echo "<tr>";
                 echo "<td class=\"soustitre2\"><p>Niveau minimum pour équiper</p></td>";
                 echo "<td><p>" . $niveau_min . "</p></td>";
                 echo "</tr>";
             }
         }
-        if ($type_objet == 2) {
+        if ($type_objet == 2)
+        {
             $req = "select obcar_armure ";
             $req = $req . "from objets_caracs,objet_generique,objets ";
             $req = $req . "where gobj_cod = $objet ";
@@ -135,25 +146,30 @@ if ($autorise == 1) {
             echo "<td><p>" . $db->f("obcar_armure") . "</p></td>";
             echo "</tr>";
         }
-        if ($type_objet == 1 and $distance == 'O') {
+        if ($type_objet == 1 and $distance == 'O')
+        {
             $req = "select obcar_chute ";
             $req = $req . "from objets_caracs,objet_generique,objets ";
             $req = $req . "where gobj_cod = $objet ";
             $req = $req . "and obj_gobj_cod = gobj_cod ";
             $req = $req . "and gobj_obcar_cod = obcar_cod ";
             $db->query($req);
-            $db->next_record();
-            echo "<tr>";
-            echo "<td class=\"soustitre2\"><p>Chute</p></td>";
-            echo "<td><p>" . $db->f("obcar_chute") . "</p></td>";
-            echo "</tr>";
+            if ($db->next_record())
+            {
+                echo "<tr>";
+                echo "<td class=\"soustitre2\"><p>Chute</p></td>";
+                echo "<td><p>" . $db->f("obcar_chute") . "</p></td>";
+                echo "</tr>";
+            }
+
         }
         echo "<tr>";
         echo "<td class=\"soustitre2\"><p>Description</p></td>";
         echo "<td><p>" . $desc . "</p></td>";
         echo "</tr>";
 
-        if (isset($bon)) {
+        if (isset($bon))
+        {
             $req = "select obon_libelle from bonus_objets where obon_cod = $bon ";
             $db->query($req);
             $db->next_record();
@@ -164,17 +180,21 @@ if ($autorise == 1) {
         }
 
         echo "</table></div>";
-    } else {
+    } else
+    {
         echo "<p>Aucun objet trouvé !";
     }
-} else {
+} else
+{
     echo "Vous n'avez pas accès au détail de cet objet !";
 }
 $retour = "inventaire.php";
-if ($origine == 'e') {
+if ($origine == 'e')
+{
     $retour = "lieu.php?methode=acheter";
 }
-if ($origine == 'a') {
+if ($origine == 'a')
+{
     $retour = "admin_echoppe_tarif.php";
 }
 echo "<a class='centrer' href=\"$retour\">Retour !</a>";
