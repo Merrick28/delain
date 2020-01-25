3096357ff387c71231bf4db76d120883b1c9311a
Merrick28 <stephane.dewitte@gmail.com>
2020-01-25 15:13:38 +0100

    bugfix enreg pos donjon

--- web/www/jeu_test/enreg_pos_donjon.php
+++ web/www/jeu_test/enreg_pos_donjon.php
@@ -1,30 +1,34 @@
-﻿<?php 
-if(!isset($db))
-	include_once "verif_connexion.php";
+﻿<?php
+//if(!isset($db))
+include_once "verif_connexion.php";
 // on regarde si le joueur est bien sur un point de passage
 
 $type_lieu = 38;
-$nom_lieu = 'un point de passage';
+$nom_lieu  = 'un point de passage';
 
 include "blocks/_test_lieu.php";
 
 if ($erreur == 0)
 {
-	$tab_lieu = $db->get_lieu($perso_cod);
-	$nom_lieu = $tab_lieu['nom'];
-	$desc_lieu = $tab_lieu['description'];
-	echo("<p><strong>$nom_lieu</strong> - $desc_lieu ");
+
+    $tab_lieu  = $db->get_lieu($perso_cod);
+    $nom_lieu  = $tab_lieu['nom'];
+    $desc_lieu = $tab_lieu['description'];
+    echo("<p><strong>$nom_lieu</strong> - $desc_lieu ");
 
     // Recherche d'une inscription dans les registres pour retour rapide en arene
     $req = "select preg_date_inscription from perso_registre where preg_perso_cod=$perso_cod ";
     $db->query($req);
-    $db->next_record();
-    $date_inscription = $db->f("preg_date_inscription");
-    if ($date_inscription!='')
+    if ($db->next_record())
     {
-        echo "<br>Vous vous êtes déjà inscrit(e) dans nos registres à la date du ".date("d/m/Y à H:i:s", strtotime($date_inscription))." <br><br>";
+        $date_inscription = $db->f("preg_date_inscription");
+        if ($date_inscription != '')
+        {
+            echo "<br>Vous vous êtes déjà inscrit(e) dans nos registres à la date du " . date("d/m/Y à H:i:s", strtotime($date_inscription)) . " <br><br>";
+        }
     }
 
-	echo("<p>Vous voyez un vieux registre poussiéreux.");
-	echo("<p><a href=\"action.php?methode=enreg_pos_donjon\">Inscrire votre nom sur le registre?</a></p>");
+
+    echo("<p>Vous voyez un vieux registre poussiéreux.");
+    echo("<p><a href=\"action.php?methode=enreg_pos_donjon\">Inscrire votre nom sur le registre?</a></p>");
 }
