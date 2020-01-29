1120b951217946c3d2b3074550928bb800d92465
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2020-01-29 09:39:38 +0100

    bonus equipement terminé

--- web/www/jeu_test/perso2_bonus.php
+++ web/www/jeu_test/perso2_bonus.php
@@ -25,7 +25,6 @@ if ($concentration->getByPerso($perso->perso_cod))
 //
 
 
-
 // Loop=0 pour bonus equipement  Loop=1 bonus temporaire
 for ($loop = 0; $loop < 2; $loop++)
 {
@@ -37,7 +36,7 @@ for ($loop = 0; $loop < 2; $loop++)
     $contenu_page .= '<div class="titre">' . ($equipement ? "Bonus d'équipement" : "Bonus temporaires") . '</div>
 <table ><tr valign="top" ><td style="padding:15px;">';
 
-    $carac_orig     = new carac_orig;
+    $carac_orig            = new carac_orig;
     $tab_carac_orig[$loop] = $carac_orig->getByPersoCumul($perso->perso_cod, $equipement);
 
     $bonus_carac = array();
@@ -59,25 +58,25 @@ for ($loop = 0; $loop < 2; $loop++)
         }
         if ($detail_carac_orig['bonus_carac'] > 0)
         {
-            $bonus_carac[$record]    = array();
-            $bonus_carac[$record][0] = $bm;
-            $bonus_carac[$record][1] = $duree;
-            $bonus_carac[$record][2] = $carac;
-            $bonus_carac[$record][3] = $corig_mode;
+            $bonus_carac[$loop][$record]    = array();
+            $bonus_carac[$loop][$record][0] = $bm;
+            $bonus_carac[$loop][$record][1] = $duree;
+            $bonus_carac[$loop][$record][2] = $carac;
+            $bonus_carac[$loop][$record][3] = $corig_mode;
         } else
         {
-            $malus_carac[$record]    = array();
-            $malus_carac[$record][0] = $bm;
-            $malus_carac[$record][1] = $duree;
-            $malus_carac[$record][2] = $carac;
-            $malus_carac[$record][3] = $corig_mode;
+            $malus_carac[$loop][$record]    = array();
+            $malus_carac[$loop][$record][0] = $bm;
+            $malus_carac[$loop][$record][1] = $duree;
+            $malus_carac[$loop][$record][2] = $carac;
+            $malus_carac[$loop][$record][3] = $corig_mode;
         }
         $record++;
     }
 
-    $tab_bonus = $perso->perso_bonus_equipement($equipement);
+    $tab_bonus[$loop] = $perso->perso_bonus_equipement($equipement);
 
-    if (count($tab_bonus) + count($bonus_carac) == 0)
+    if (count($tab_bonus[$loop]) + count($bonus_carac[$loop]) == 0)
     {
         $contenu_page .= '<p>Vous n’avez aucun bonus ' . ($loop == 0 ? "d'équipement " : "") . 'en ce moment.</p>';
     } else
@@ -88,22 +87,19 @@ for ($loop = 0; $loop < 2; $loop++)
 	<td class="soustitre2"><strong>Échéance</strong></td>
 	<td class=""></td>
 	</tr>';
-        foreach ($tab_bonus as $detail_bonus)
+        foreach ($tab_bonus[$loop] as $key => $detail_bonus)
         {
             if (is_file(__DIR__ . "/../images/interface/bonus/" . $db->f('tbonus_libc') . ".png"))
             {
-                $img = '<img src="/../images/interface/bonus/' . $db->f('tbonus_libc') . '.png">';
+                $img = '/../images/interface/bonus/' . $detail_bonus['tbonus_libc'] . '.png';
             } else
             {
-                $img = '<img src="/../images/interface/bonus/MALUS.png">';
+                $img = '/../images/interface/bonus/MALUS.png';
             }
-            $contenu_page .= '<tr><td class="soustitre2"><strong>' . $detail_bonus['tonbus_libelle'] . $detail_bonus['bonus_mode'] == "C" ? " (cumulatif)" : "" . '</strong></td>';
-            $signe        = ($detail_bonus['bonus_valeur'] >= 0) ? '+' : '';
-            $contenu_page .= '<td><div style="text-align:center;">' . $signe . $detail_bonus['bonus_valeur'] . '</div></td>
-		<td><div style="text-align:center;">' . ($detail_bonus['bonus_mode'] == "E" ? " Equipement" : $detail_bonus['bonus_nb_tours'] . " tour(s)") . '</div></td>
-		<td style="text-align:center">' . $img . '</td></tr>';
+            $tab_bonus[$loop][$key]['img'] = $img;
+
         }
-        foreach ($bonus_carac as $bonus)
+        foreach ($bonus_carac[$loop] as $key => $bonus)
         {
             $valeur     = $bonus[0];
             $duree      = $bonus[1];
@@ -126,14 +122,16 @@ for ($loop = 0; $loop < 2; $loop++)
                     $lib_carac = 'Dextérité';
                     break;
             }
+            $bonus_carac[$loop][$key]['lib_carac'] = $lib_carac;
             if (is_file(__DIR__ . "/../images/interface/bonus/" . $carac . ".png"))
             {
-                $img = '<img src="/../images/interface/bonus/' . $carac . '.png">';
+                $img = '/../images/interface/bonus/' . $carac . '.png';
             } else
             {
-                $img = '<img src="/../images/interface/bonus/MALUS.png">';
+                $img = "/../images/interface/bonus/MALUS.png";
             }
-            $contenu_page .= "<tr><td class='soustitre2'><strong>$lib_carac</strong></td>
+            $bonus_carac[$loop][$key]['img'] = $img;
+            $contenu_page                    .= "<tr><td class='soustitre2'><strong>$lib_carac</strong></td>
 			<td><div style='text-align:center;'>$signe" . "$valeur</div></td>
 			<td><div style='text-align:center;'>" . ($corig_mode == "Equipement" ? "Equipement" : $duree) . "</div></td>
 			<td><div style='text-align:center;'>$img</div></td>
@@ -144,9 +142,9 @@ for ($loop = 0; $loop < 2; $loop++)
 
     $contenu_page .= '</td><td style="padding:15px;">';
 
-    $tab_malus = $perso->perso_malus_equipement($equipement);
+    $tab_malus[$loop] = $perso->perso_malus_equipement($equipement);
 
-    if (count($tab_malus) + count($malus_carac) == 0)
+    if (count($tab_malus[$loop]) + count($malus_carac[$loop]) == 0)
     {
         $contenu_page .= '<p>Vous n’avez aucun malus ' . ($loop == 0 ? "d'équipement " : "") . 'en ce moment.</p>';
     } else
@@ -157,23 +155,18 @@ for ($loop = 0; $loop < 2; $loop++)
 	<td class="soustitre2"><strong>Échéance</strong></td>
 	<td class=""></td>
 	</tr>';
-        foreach ($tab_malus as $detail_malus)
+        foreach ($tab_malus[$loop] as $key => $detail_malus)
         {
             if (is_file(__DIR__ . "/../images/interface/bonus/" . $detail_malus['tbonus_libc'] . ".png"))
             {
-                $img = '<img src="/../images/interface/bonus/' . $detail_malus['tbonus_libc'] . '.png">';
+                $img = '/../images/interface/bonus/' . $detail_malus['tbonus_libc'] . '.png';
             } else
             {
-                $img = '<img src="/../images/interface/bonus/MALUS.png">';
+                $img = '/../images/interface/bonus/MALUS.png';
             }
-
-            $contenu_page .= '<tr><td class="soustitre2"><strong>' . $detail_malus['tonbus_libelle'] . ($detail_malus['bonus_mode'] == "C" ? " (cumulatif)" : "") . '</strong></td>';
-            $signe        = ($detail_malus['bonus_valeur'] >= 0) ? '+' : '';
-            $contenu_page .= '<td><div style="text-align:center;">' . $signe . $detail_malus['bonus_valeur'] . '</div></td>
-		<td><div style="text-align:center;">' . ($detail_malus['bonus_mode'] == "E" ? " Equipement" : $detail_malus['bonus_nb_tours'] . " tour(s)") . '</div></td>
-		<td style="text-align:center">' . $img . '</td></tr>';
+            $tab_malus[$loop][$key]['img'] = $img;
         }
-        foreach ($malus_carac as $malus)
+        foreach ($malus_carac[$loop] as $key => $malus)
         {
             $valeur    = $malus[0];
             $duree     = $malus[1];
@@ -195,13 +188,15 @@ for ($loop = 0; $loop < 2; $loop++)
                     $lib_carac = 'Dextérité';
                     break;
             }
+            $malus_carac[$loop][$key]['lib_carac'] = $lib_carac;
             if (is_file(__DIR__ . "/../images/interface/bonus/" . $carac . ".png"))
             {
-                $img = '<img src="/../images/interface/bonus/' . $carac . '.png">';
+                $img = '/../images/interface/bonus/' . $carac . '.png';
             } else
             {
-                $img = '<img src="/../images/interface/bonus/MALUS.png">';
+                $img = '/../images/interface/bonus/MALUS.png';
             }
+            $malus_carac[$loop][$key]['img'] = $img;
             $contenu_page .= "<tr><td class='soustitre2'><strong>$lib_carac</strong></td>
 			<td><div style='text-align:center;'>$signe" . "$valeur</div></td>
 			<td><div style='text-align:center;'>" . ($corig_mode == "Equipement" ? "Equipement" : $duree) . "</div></td>
@@ -219,7 +214,12 @@ $options_twig = array(
     'PERSO'             => $perso,
     'PHP_SELF'          => $PHP_SELF,
     'CONENTRATION'      => $concentration,
-    'HAS_CONCENTRATION' => $has_concentration
+    'HAS_CONCENTRATION' => $has_concentration,
+    'TAB_CARAC_ORIG'    => $tab_carac_orig,
+    'TAB_BONUS'         => $tab_bonus,
+    'TAB_MALUS'         => $tab_malus,
+    'BONUS_CARAC'       => $bonus_carac,
+    'MALUS_CARAC'       => $malus_carac
 
 
 );
