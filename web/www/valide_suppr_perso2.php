5200f76c28bb56ea6d0e02f40cedf15990598c1b
Merrick28 <stephane.dewitte@gmail.com>
2020-02-04 18:15:54 +0100

    passage en pdo

--- web/www/valide_suppr_perso2.php
+++ web/www/valide_suppr_perso2.php
@@ -4,7 +4,7 @@ include "jeu_test/verif_connexion.php";
 require_once G_CHE . "/includes/base_delain.php";
 
 $perso_cible = $_REQUEST['perso'];
-$pdo = new bddpdo();
+$pdo         = new bddpdo();
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,35 +17,50 @@ $pdo = new bddpdo();
 <body background="../images/fond5.gif">
 <div class="bordiv">
     <?php
-    $erreur = 0;
-    $req = 'select (perso_dcreat + \'24 hours\'::interval) > now() as test, perso_type_perso from perso where perso_cod = :perso';
-    $stmt = $pdo->prepare($req);
-    $stmt = $pdo->execute(array(":perso" => $perso_cible),$stmt);
-    if(!$result = $stmt->fetch())
+
+    $erreur          = 0;
+    $perso_a_effacer = new perso;
+    if (!$perso_a_effacer->charge($perso_cible))
+    {
+        die('erreur sur le chargement de perso');
+    }
+
+    $perso_dcreat = new DateTime($perso->perso_dcreat);
+    date_add($perso_dcreat, date_interval_create_from_date_string('1 days'));
+    $now = date_create();
+    if ($perso_dcreat > $now)
     {
         $erreur = 1;
     }
 
 
-
-    if ($result['test'] == 't') {
-        $erreur = 1;
-    }
-    $perso_a_effacer = new perso;
-    $perso_a_effacer->charge($perso_cible);
     $type_perso = $perso_a_effacer->perso_type_perso;
 
-    if ($type_perso == 3) {
-        $req = "select pcompt_compt_cod from perso_familier,perso_compte where pfam_familier_cod = $perso_cible and pfam_perso_cod = pcompt_perso_cod";
-        $db->query($req);
-        $db->next_record();
-        $compt_fam = $db->f("pcompt_compt_cod");
+    if ($type_perso == 3)
+    {
+        $req       =
+            "select pcompt_compt_cod from perso_familier,perso_compte where pfam_familier_cod = :perso_cible 
+                            and pfam_perso_cod = pcompt_perso_cod";
+        $stmt      = $pdo->prepare($req);
+        $stmt      = $pdo->execute(array(":perso_cible" => $perso_cible), $stmt);
+        $result    = $stmt->fetch();
+        $compt_fam = $result['pcompt_compt_cod'];
     } else
+    {
         $compt_fam = -1;
+    }
 
-    $req = "select pcompt_perso_cod, perso_type_perso from perso_compte, perso where pcompt_perso_cod = $perso_cible and pcompt_compt_cod = $compt_cod and pcompt_perso_cod = perso_cod";
-    $db->query($req);
-    if ($db->nf() == 0 and $type_perso != 3) // Non rattaché au compte, pas un familier.
+
+    $req  =
+        "select pcompt_perso_cod, perso_type_perso 
+            from perso_compte, perso 
+            where pcompt_perso_cod = $perso_cible 
+              and pcompt_compt_cod = $compt_cod 
+              and pcompt_perso_cod = perso_cod";
+    $stmt = $pdo->prepare($req);
+    $stmt = $pdo->execute(array(":perso_cible" => $perso_cible,
+                                ":compt_cod"   => $compt_cod), $stmt);
+    if ($result = $stmt->fetch() and $type_perso != 3) // Non rattaché au compte, pas un familier.
     {
         echo "Vous êtes en train de tenter de supprimer un personnage qui n’est pas rattaché à votre compte !";
     } else if ($type_perso == 3 and $compt_cod != $compt_fam) // Un familier, mais rattaché à un perso d’un autre compte
@@ -57,45 +72,61 @@ $pdo = new bddpdo();
         'laissez-le donc vivre un peu plus...';
     } else if ($type_perso == 2) // Cas d’un monstre contrôlé par le compte. Pas de suppression, juste un détachement.
     {
-        $req1 = "select case when (now() - pcompt_date_attachement) < '1 day'::interval then 1 else 0 end as trop_recent, perso_actif
-		from perso
-		inner join perso_compte on pcompt_perso_cod = perso_cod
-		where perso_cod = $perso_cible";
-        $db->query($req1);
-        $db->next_record();
-        $trop_recent = $db->f('trop_recent');
-        $actif = $db->f('perso_actif');
-        if ($trop_recent == 1 && $actif == 'O') {
+        $perso_compte = new perso_compte;
+        $perso_compte->get_by_perso($perso_cible);
+
+        $perso_dcreat = new DateTime($perso_compte->pcompt_date_attachement);
+        date_add($perso_dcreat, date_interval_create_from_date_string('1 days'));
+
+        if ($perso_dcreat > $now)
+        {
             echo '<p>Ce monstre vous a été affecté trop récemment, gardez-le au moins 24 heures !</p>';
-        } else {
-            $req1 = "select relache_monstre_4e_perso($perso_cible, 2::smallint) as resultat";
-            $db->query($req1);
-            $db->next_record();
-            echo '<p>' . $db->f("resultat") . '</p>';
-        }
-    } else {
-        $req1 = "update perso set perso_nom = perso_nom||'_inactif' where perso_cod = $perso_cible ";
-        $db->query($req1);
-        $req1 = "delete from lock_combat where lock_cible = $perso_cible ";
-        $db->query($req1);
-        $req1 = "delete from lock_combat where lock_attaquant = $perso_cible ";
-        $db->query($req1);
-        $req1 = "delete from perso_familier where pfam_familier_cod = $perso_cible ";
-        $db->query($req1);
-        $req1 = "select pfam_familier_cod from perso_familier where pfam_perso_cod = $perso_cible ";
-        $db->query($req1);
-        if ($db->nf() != 0) {
-            $req = "update perso set perso_type_perso = 2 where perso_cod in ";
-            $req = $req . "(select pfam_familier_cod from perso_familier where pfam_perso_cod = $perso_cible) ";
-            $db->query($req);
         }
 
-        $req = "insert into ligne_evt (levt_tevt_cod,levt_date,levt_type_per1,levt_perso_cod1,levt_texte,levt_lu,levt_visible) ";
-        $req = $req . "values (23,now(),1,$perso_cible,'Le personnage a été supprimé !','O','O') ";
-        $db->query($req);
+        {
+            $req1   = "select relache_monstre_4e_perso($perso_cible, 2::smallint) as resultat";
+            $stmt   = $pdo->prepare($req1);
+            $stmt   = $pdo->execute(array(":cible" => $perso_cible), $stmt);
+            $result = $stmt->fetch();
+            echo '<p>' . $result['resultat'] . '</p>';
+        }
+    } else
+    {
+        $perso_a_effacer->perso_nom   = $perso_a_effacer->perso_nom . '_inactif';
+        $perso_a_effacer->perso_actif = 'N';
+        $perso_a_effacer->stocke();
 
-        $req2 = "update perso set perso_actif = 'N' where perso_cod = $perso_cible ";
-        $db->query($req2);
+        $req1   = "update perso set perso_nom = perso_nom||'_inactif' where perso_cod = :perso ";
+        $stmt   = $pdo->prepare($req1);
+        $stmt   = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+        $req1   = "delete from lock_combat where lock_cible = :perso ";
+        $stmt   = $pdo->prepare($req1);
+        $stmt   = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+        $req1   = "delete from lock_combat where lock_attaquant = :perso ";
+        $stmt   = $pdo->prepare($req1);
+        $stmt   = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+        $req1   = "delete from perso_familier where pfam_familier_cod = :perso ";
+        $stmt   = $pdo->prepare($req1);
+        $stmt   = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+        $req1   = "select pfam_familier_cod from perso_familier where pfam_perso_cod = :perso ";
+        $stmt   = $pdo->prepare($req1);
+        $stmt   = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+        $allfam = $stmt->fetchAll();
+        if (count($allfam) != 0)
+        {
+            $req  = "update perso set perso_type_perso = 2 where perso_cod in 
+            (select pfam_familier_cod from perso_familier where pfam_perso_cod = :perso) ";
+            $stmt = $pdo->prepare($req);
+            $stmt = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+        }
+
+        $req      =
+            "insert into ligne_evt (levt_tevt_cod,levt_date,levt_type_per1,levt_perso_cod1,levt_texte,levt_lu,levt_visible) 
+            values (23,now(),1,:perso,'Le personnage a été supprimé !','O','O') ";
+        $stmt     = $pdo->prepare($req);
+            $stmt = $pdo->execute(array(":perso" => $perso_cible), $stmt);
+
+
         echo("<p>Votre perso a été supprimé !");
     }
     echo "<a class='centrer' href=\"jeu_test/switch.php\">Retour !</a>";
--- web/www/verif_vote.php
+++ web/www/verif_vote.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 ini_set('include_path', '.:/home/delain/delain/web/phplib-7.4a/php:/home/delain/delain/web/www/includes');
 
 include_once "delain_header.php";
 include "classes.php";
-$db = new base_delain;
+$pdo = new bddpdo();
 
 $IpReq1 = "SELECT icompt_compt_ip,  compte_vote_compte_cod,compte_vote_cod,compte_vote_ip_compte
             FROM public.compte_ip
@@ -14,43 +14,43 @@ $IpReq2 = "SELECT icompt_compt_ip,  compte_vote_compte_cod,compte_vote_cod,compt
             INNER JOIN public.compte_vote_ip ON public.compte_ip.icompt_cod = public.compte_vote_ip.compte_vote_icompt_cod where  public.compte_vote_ip.compte_vote_verifier = FALSE 
             and public.compte_vote_ip.compte_vote_date = (current_date - 1)  FETCH NEXT 100 ROWS ONLY";
 
-$ips = array();
-$Compte_code_ips = array();
+$ips                = array();
+$Compte_code_ips    = array();
 $Compte_vote_codeId = array();
-$db->query($IpReq1);
-while ($db->next_record())
+$stmt               = $pdo->query($IpReq1);
+while ($result = $stmt->fetch())
 {
-    $Code_ip = trim($db->f('compte_vote_ip_compte'));
+    $Code_ip = trim($result['compte_vote_ip_compte']);
     array_push($ips, $Code_ip);
-    $Code_compte_vote = $db->f('compte_vote_compte_cod');
-    $compte_vote_codeid = $db->f('compte_vote_cod');
-    $Compte_code_ips[$Code_ip] = $Code_compte_vote;
+    $Code_compte_vote             = $result['compte_vote_compte_cod'];
+    $compte_vote_codeid           = $result['compte_vote_cod'];
+    $Compte_code_ips[$Code_ip]    = $Code_compte_vote;
     $Compte_vote_codeId[$Code_ip] = $compte_vote_codeid;
 }
 
 $postdata = http_build_query(
     array(
         'nomFonction' => 'IPVote',
-        'idJeu' => 715,
-        'dateVote' => date("Y-m-d"),
-        'IP' => $ips
+        'idJeu'       => 715,
+        'dateVote'    => date("Y-m-d"),
+        'IP'          => $ips
     )
 );
 
 if (count($ips) != 0)
 {
     $opts = array('http' =>
-        array(
-            'method' => 'POST',
-            'header' => 'Content-type: application/x-www-form-urlencoded',
-            'content' => $postdata
-        )
+                      array(
+                          'method'  => 'POST',
+                          'header'  => 'Content-type: application/x-www-form-urlencoded',
+                          'content' => $postdata
+                      )
     );
 
     $context = stream_context_create($opts);
 
-    $result = file_get_contents('http://www.jeux-alternatifs.com/API.php', false, $context);
-    $list = json_decode(substr($result, 3), true);
+    $result      = file_get_contents('http://www.jeux-alternatifs.com/API.php', false, $context);
+    $list        = json_decode(substr($result, 3), true);
     $listContenu = $list['contenu'];
     if ($listContenu != null)
     {
@@ -69,35 +69,42 @@ if (count($ips) != 0)
                 // si c'est à deux c'est qu'il a cliqué mais pas voté pour delain
                 if ($etatVote != 1)
                 {
-                    $reqUpdate = "UPDATE public.compte_vote_ip SET  compte_vote_verifier= true,compte_vote_pour_delain = false WHERE compte_vote_compte_cod = " . $compteVoteCod . " and compte_vote_ip_compte='" . $IP . "'";
-                    $db->query($reqUpdate);
-                }
-                else
+                    $reqUpdate =
+                        "UPDATE public.compte_vote_ip SET  compte_vote_verifier= true,compte_vote_pour_delain = false WHERE compte_vote_compte_cod = :compte and compte_vote_ip_compte=:ip";
+                    $stmt      = $pdo->prepare($reqUpdate);
+                    $stmt      = $pdo->execute(array(":compte" => $compteVoteCod,
+                                                     ":ip"     => $IP), $stmt);
+
+                } else
                 {
 
-                    $reqUpdate = "UPDATE public.compte_vote_ip SET  compte_vote_verifier= true,compte_vote_pour_delain = true WHERE compte_vote_compte_cod = " . $compteVoteCod . "and compte_vote_ip_compte='" . $IP . "'";
-                    $db->query($reqUpdate);
+                    $reqUpdate =
+                        "UPDATE public.compte_vote_ip SET  compte_vote_verifier= true,compte_vote_pour_delain = true WHERE compte_vote_compte_cod = :compte and compte_vote_ip_compte=:ip";
+                    $stmt      = $pdo->prepare($reqUpdate);
+                    $stmt      = $pdo->execute(array(":compte" => $compteVoteCod,
+                                                     ":ip"     => $IP), $stmt);
 
                     $reqselectVotecompte = "SELECT compte_vote_cod, compte_vote_total_px_gagner, compte_vote_nbr, 
                                     compte_vote_compte_cod
-                               FROM public.compte_vote where compte_vote_compte_cod=" . $compteVoteCod;
-                    $db->query($reqselectVotecompte);
-                    // s'il n'y a rien il faut creer la ligne
-                    // sinon on update
-                    if ($db->next_record())
-                    {
+                               FROM public.compte_vote where compte_vote_compte_cod=:compte";
+                    $stmt                = $pdo->prepare($reqselectVotecompte);
+                    $stmt                = $pdo->execute(array(":compte" => $compteVoteCod), $stmt);
 
+
+                    if ($result = $stmt->fetch())
+                    {
                         // mise à jours du compte
+                        $reqCompteVote =
+                            "UPDATE public.compte_vote SET   compte_vote_nbr=(compte_vote_nbr+1) WHERE   compte_vote_compte_cod=:compte";
 
-                        $reqCompteVote = "UPDATE public.compte_vote SET   compte_vote_nbr=(compte_vote_nbr+1) WHERE   compte_vote_compte_cod=" . $compteVoteCod;
-                        $db->query($reqCompteVote);
-                    }
-                    else
+                    } else
                     {
 
-                        $reqCompteVote = "INSERT INTO public.compte_vote( compte_vote_nbr, compte_vote_compte_cod) VALUES (1, " . $compteVoteCod . ")";
-                        $db->query($reqCompteVote);
+                        $reqCompteVote =
+                            "INSERT INTO public.compte_vote( compte_vote_nbr, compte_vote_compte_cod) VALUES (1, :compte )";
                     }
+                    $stmt = $pdo->prepare($reqCompteVote);
+                    $stmt = $pdo->execute(array(":compte" => $compteVoteCod), $stmt);
 
                 }
 
@@ -114,85 +121,97 @@ if (count($ips) != 0)
 $dateDuJour = date("Y-m-d");
 $dateDuMois = date("Y-m");
 // on regarde par rapport au mois
-$reqVoteList = "SELECT vote_list_date FROM public.vote_list where to_char(vote_list_date, 'yyyy-mm') ='" . $dateDuMois . "'";
-$db->query($reqVoteList);
+$reqVoteList =
+    "SELECT vote_list_date FROM public.vote_list where to_char(vote_list_date, 'yyyy-mm') = :date";
+$stmt        = $pdo->prepare($reqVoteList);
+$stmt        = $pdo->execute(array(":date" => $dateDuMois), $stmt);
+
 // on ne fait quelque chose que s'il n'y a pas de requete sinon cela veut dire qu'on est deja passé ce mois si
-if (!$db->next_record())
+if (!$result = $stmt->fetch())
 {
 
     // on commence par mettre a jour (#LAG pour eviter une multiple distribution)
-    $repUpdateVoteList = " INSERT INTO public.vote_list(vote_list_date) VALUES ('" . $dateDuJour . "'::date)";
-    $db->query($repUpdateVoteList);
+    $repUpdateVoteList = " INSERT INTO public.vote_list(vote_list_date) VALUES (:date)";
+    $stmt              = $pdo->prepare($repUpdateVoteList);
+    $stmt              = $pdo->execute(array(":date" => $dateDuMois), $stmt);
     // puis on récupere tous les personnes qui ont voté ce mois ci et on fonction du nombre de vote on donne les xp correspondants.
     $requGetCompteVote = "select compte_vote_compte_cod FROM public.compte_vote";
-    $db->query($requGetCompteVote);
+    $stmt              = $pdo->query($requGetCompteVote);
+
+    $ReqNbrVote = "select count(*)as nbrvote from public.compte_vote_ip where to_char(compte_vote_date, 'yyyy-mm') 
+				= :mois and  compte_vote_pour_delain=true and compte_vote_compte_cod = :compte";
+    $stmt2      = $pdo->prepare($ReqNbrVote);
+
+    $ReqGainXp = "select vote_xp_gain_xp from public.vote_xp where vote_xp_nombre_vote= :vote";
+    $stmt3     = $pdo->prepare($ReqGainXp);
+
+    $reqPerso = "select pcompt_perso_cod from public.perso_compte where pcompt_compt_cod= :compte";
+    $stmt4    = $pdo->prepare($reqPerso);
+
+    $ReqSelectFafaCod = "select pfam_familier_cod from public.perso 
+                                    inner join public.perso_familier ON public.perso_familier.pfam_perso_cod = public.perso.perso_cod
+                                    where public.perso_familier.pfam_perso_cod = :perso";
+    $stmt5            = $pdo->prepare($ReqSelectFafaCod);
+
+    $ReqUpdateCompteVote = "UPDATE public.compte_vote
+                SET compte_vote_total_px_gagner=(compte_vote_total_px_gagner + :gain)
+              WHERE compte_vote_compte_cod= :compte";
+    $stmt6               = $pdo->prepare($ReqUpdateCompteVote);
     // pour chacun des comptes on ajoute l'xp
-    $db2 = new base_delain;
-    while ($db->next_record())
+
+    while ($result = $stmt->fetch())
     {
-        $compteCod = $db->f('compte_vote_compte_cod');
+        $compteCod = $result['compte_vote_compte_cod'];
 
         $moisAnnee = date('Y-m', mktime(12, 0, 0, date("m") - 1, 1, date("Y")));
         // il faut récuperer dans compte_vote_ip pour le nombre de vote
-        $ReqNbrVote = "select count(*)as nbrvote from public.compte_vote_ip where to_char(compte_vote_date, 'yyyy-mm') 
-				= '" . $moisAnnee . "' and  compte_vote_pour_delain=true and compte_vote_compte_cod = " . $compteCod;
 
-        $db2->query($ReqNbrVote);
-        $db2->next_record();
-        $nbrVote = $db2->f('nbrvote');
+        $stmt2   = $pdo->execute(array(
+                                     ":mois"   => $moisAnnee,
+                                     ":compte" => $compteCod
+                                 ), $stmt2);
+        $result2 = $stmt2->fetch();
+        $nbrVote = $result2['nbrvote'];
 
         // il n'y a plus qu'a récuperer l'xp a faire gagner par perso et les perso puis ajouter l'xp gagné
-        $ReqGainXp = "select vote_xp_gain_xp from public.vote_xp where vote_xp_nombre_vote=" . $nbrVote;
-        $db2->query($ReqGainXp);
-        $db2->next_record();
-        $GainXp = 1 * $db2->f('vote_xp_gain_xp');
+        $stmt3   = $pdo->execute(array(":vote" => $nbrVote), $stmt3);
+        $result3 = $stmt3->fetch();
+        $GainXp  = 1 * $result3['vote_xp_gain_xp'];
         echo 'gain: ' . $GainXp;
         // pour chacun des perso du comptes, on rajoute  l'xp gagné
-        $reqPerso = "select pcompt_perso_cod from public.perso_compte where pcompt_compt_cod=" . $compteCod;
-        $db2->query($reqPerso);
-        $db3 = new base_delain;
-        while ($db2->next_record())
+
+        $stmt4 = $pdo->execute(array(":compte" => $compteCod), $stmt4);
+        while ($result4 = $stmt4->fetch())
         {
-            $codPerso = $db2->f('pcompt_perso_cod');
+            $codPerso  = $result4['pcompt_perso_cod'];
+            $tempperso = new perso;
+            $tempperso->charge($codPerso);
+            $tempperso->perso_px = $tempperso->perso_px + $GainXp;
+            $tempperso->stocke();
+            unset($tempperso);
 
-            $ReqUpdatePerso = "UPDATE public.perso
-                            SET perso_px = (perso_px +" . $GainXp . ")
-                          WHERE perso_cod = " . $codPerso;
-
-            $db3->query($ReqUpdatePerso);
 
             // et on cherche s'il y a un fafa rattaché, si oui, on ajoute aussi de l'xp! Parce qu'on est sympas ^^
-            $ReqSelectFafaCod = "select pfam_familier_cod from public.perso 
-                                    inner join public.perso_familier ON public.perso_familier.pfam_perso_cod = public.perso.perso_cod
-                                    where public.perso_familier.pfam_perso_cod = " . $codPerso;
+            $stmt5 = $pdo->execute(array(":perso" => $codPerso), $stmt5);
 
-            $db3->query($ReqSelectFafaCod);
-            if ($db3->next_record())
+
+            if ($result5 = $stmt->fetch())
             {
-                $fafaCod = $db3->f('pfam_familier_cod');
-                $ReqUpdateFafa = "UPDATE public.perso
-                            SET perso_px = (perso_px +" . $GainXp . ")
-                          WHERE perso_cod = " . $fafaCod;
-                $db3->query($ReqUpdateFafa);
+                $tempperso = new perso;
+                $tempperso->charge($result5['pfam_familier_cod']);
+                $tempperso->perso_px = $tempperso->perso_px + $GainXp;
+                $tempperso->stocke();
+                unset($tempperso);
+
             }
 
 
         }
 
         // une fois la mise a jour faite, on met a jour compte_vote
-        $ReqUpdateCompteVote = "UPDATE public.compte_vote
-                SET compte_vote_total_px_gagner=(compte_vote_total_px_gagner + " . $GainXp . ")
-              WHERE compte_vote_compte_cod=" . $compteCod;
-        $db3->query($ReqUpdateCompteVote);
-
-
+        $stmt6 = $pdo->execute(array(":gain"   => $GainXp,
+                                     ":compte" => $compteCod), $stmt6);
     }
-	//#LAG Déjà fait avant la boucle
-    //// on cree à la bonne date
-    //$ReqCreationVoteList = "INSERT INTO public.vote_list(
-    //         vote_list_date)
-    //             VALUES ( '" . $dateDuJour . "'::date)";
-    //$db->query($ReqCreationVoteList);
 
 }
 
