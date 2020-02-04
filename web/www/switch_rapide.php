7789f44582569d27ef13e73cc973a4044893eff6
Merrick28 <stephane.dewitte@gmail.com>
2020-02-04 10:03:05 +0100

    passage en pdo

--- web/www/switch_rapide.php
+++ web/www/switch_rapide.php
@@ -16,39 +16,41 @@ if (!$verif_auth)
 }
 // normalement, les objets $compte et $perso sont déjà chargés par la page ident.php
 
-$frameless = ($compte->compt_frameless == 'O');
+$frameless        = ($compte->compt_frameless == 'O');
 $autorise_monstre = ($compte->autorise_4e_monstre() == 't');
 
 
 $num_resultat = 0;
-$db = new base_delain;
+$pdo          = new bddpdo();
 $requete
-    = "SELECT perso_cod, perso_nom, coalesce(perso_mortel, 'N') AS perso_mortel, 
+              = "SELECT perso_cod, perso_nom, coalesce(perso_mortel, 'N') AS perso_mortel, 
 			perso_dlt, dlt_passee(perso_cod) dlt_passee
-		FROM perso WHERE perso_cod = " . $perso_cod;
-$db->query($requete);
-$num_resultat = $db->nf();
-if ($num_resultat>0)
+		FROM perso WHERE perso_cod = :perso";
+$stmt         = $pdo->prepare($requete);
+$stmt         = $pdo->execute(array(":perso" => $perso_cod), $stmt);
+
+
+$perso = new perso;
+if($perso->charge($perso_cod))
 {
-    $db->next_record();
-    $perso_dlt_passee = $db->f('dlt_passee');
+    $perso_dlt_passee = $perso->dlt_passee();
 } else
 {
     $perso_dlt_passee = 0;
 }
-$nom = $perso->perso_nom;
-$perso_nom = str_replace(chr(39), " ", $nom);
-$maintenant = date("d/m/Y H:i:s");
+$nom          = $perso->perso_nom;
+$perso_nom    = str_replace(chr(39), " ", $nom);
+$maintenant   = date("d/m/Y H:i:s");
 $perso_mortel = $perso->perso_mortel;
-$num_perso = $perso->perso_cod;
-$perso_cod = $num_perso;
-$autorise = 0;
-$type_perso = $perso->perso_type_perso;
+$num_perso    = $perso->perso_cod;
+$perso_cod    = $num_perso;
+$autorise     = 0;
+$type_perso   = $perso->perso_type_perso;
 if ($type_perso == 1 || ($type_perso == 2 && $autorise_monstre))
 {
     // on va quand même charger le perso_compte
     $pcompt = new perso_compte();
-    $tab = $pcompt->getBy_pcompt_perso_cod($perso->perso_cod);
+    $tab    = $pcompt->getBy_pcompt_perso_cod($perso->perso_cod);
     if ($tab !== false)
     {
         // On a trouvé un perso_compte pour ce perso
@@ -60,8 +62,8 @@ if ($type_perso == 1 || ($type_perso == 2 && $autorise_monstre))
     }
 } elseif ($type_perso == 3)
 {
-    $pfam = new perso_familier();
-    $pcompt = new perso_compte();
+    $pfam    = new perso_familier();
+    $pcompt  = new perso_compte();
     $tab_fam = $pfam->getBy_pfam_familier_cod($perso->perso_cod);
     if ($tab_fam !== false)
     {
@@ -120,7 +122,6 @@ if ($autorise != 1)
     echo '</div>';
 
 
-
     $contenu_page = ob_get_contents();
     ob_end_clean();
 
@@ -139,47 +140,45 @@ if ($autorise != 1)
         'CONTENU_PAGE' => $contenu_page
 
     );
-    echo $template->render(array_merge($var_twig_defaut,$options_twig_defaut, $options_twig));
+    echo $template->render(array_merge($var_twig_defaut, $options_twig_defaut, $options_twig));
     die();                  // Ne pas continuer en cas d'erreur
 }
 
 // Récupération de la racine du site
-$root = 'https://' . $_SERVER['HTTP_HOST'] ;
+$root = 'https://' . $_SERVER['HTTP_HOST'];
 if ($perso_dlt_passee != 0)
 {
     // Le quick-switch ne marche pas, il faut d'abord activer la DLT!
     header("Location: /validation_login3.php");
-}
-else if (!in_array( substr(str_replace("http://", "https://", $_REQUEST["url"]), strlen($root)) , array(
-        "/jeu_test/perso2.php",
-        "/jeu_test/frame_vue.php",
-        "/jeu_test/evenements.php",
-        "/jeu_test/inventaire.php",
-        "/jeu_test/ramasser.php",
-        "/jeu_test/transactions2.php",
-        "/jeu_test/deplacement.php",
-        "/jeu_test/combat.php",
-        "/jeu_test/magie.php",
-        "/jeu_test/choix_voie_magique.php",
-        "/jeu_test/enchantement_general.php",
-        "/jeu_test/objets/pioche.php",
-        "/jeu_test/enluminure_general.php",
-        "/jeu_test/concentration.php",
-        "/jeu_test/messagerie2.php",
-        "/jeu_test/guilde.php",
-        "/jeu_test/groupe.php",
-        "/jeu_test/quete_auto.php"
-    )))
+} else if (!in_array(substr(str_replace("http://", "https://", $_REQUEST["url"]), strlen($root)), array(
+    "/jeu_test/perso2.php",
+    "/jeu_test/frame_vue.php",
+    "/jeu_test/evenements.php",
+    "/jeu_test/inventaire.php",
+    "/jeu_test/ramasser.php",
+    "/jeu_test/transactions2.php",
+    "/jeu_test/deplacement.php",
+    "/jeu_test/combat.php",
+    "/jeu_test/magie.php",
+    "/jeu_test/choix_voie_magique.php",
+    "/jeu_test/enchantement_general.php",
+    "/jeu_test/objets/pioche.php",
+    "/jeu_test/enluminure_general.php",
+    "/jeu_test/concentration.php",
+    "/jeu_test/messagerie2.php",
+    "/jeu_test/guilde.php",
+    "/jeu_test/groupe.php",
+    "/jeu_test/quete_auto.php"
+)))
 {
-     // par sécurité, on switch sur la vue.
-   // // echo "<pre>"; print_r($root); echo "</pre>";
-   // die($_SERVER["HTTP_ORIGIN"]." => ".substr($_REQUEST["url"], strlen($_SERVER["HTTP_ORIGIN"])));
+    // par sécurité, on switch sur la vue.
+    // // echo "<pre>"; print_r($root); echo "</pre>";
+    // die($_SERVER["HTTP_ORIGIN"]." => ".substr($_REQUEST["url"], strlen($_SERVER["HTTP_ORIGIN"])));
     header("Location: /jeu_test/frame_vue.php");
-}
-else
+} else
 {
-     // retourner sur l'url en cours de consultation avant le quick-switch
-    header("Location: ".$_REQUEST["url"]);
+    // retourner sur l'url en cours de consultation avant le quick-switch
+    header("Location: " . $_REQUEST["url"]);
 }
 
 ?>
