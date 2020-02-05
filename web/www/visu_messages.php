807859559bb30bea36cedc79a23304fbbabb9bd6
Stephane DEWITTE <stephane.dewitte@gmail.com>
2020-02-05 14:11:57 +0100

    Passage en pdo du rep www

--- web/www/visu_messages.php
+++ web/www/visu_messages.php
@@ -7,9 +7,7 @@ $contenu_page = '';
 $titre_page   = '';
 
 $erreur = empty($_REQUEST['visu_perso']) || empty($compt_cod);
-if (!$erreur)
-{
-
+if (!$erreur) {
     $req_compte    = "select pcompt_compt_cod from perso_compte
 		where pcompt_perso_cod = :perso
 		or pcompt_perso_cod = ( select pfam_perso_cod from perso_familier where pfam_familier_cod = :perso)";
@@ -20,8 +18,7 @@ if (!$erreur)
 
     $erreur = ($compte_trouve === false || $compte_trouve != $compt_cod);
 
-    if ($erreur)    // on est peut-être dans le cas d’un sitting
-    {
+    if ($erreur) {    // on est peut-être dans le cas d’un sitting
         $req_sitting    = "select csit_compte_sitteur from compte_sitting
 			inner join perso_compte on pcompt_compt_cod = csit_compte_sitte
 			where pcompt_perso_cod = :perso
@@ -34,29 +31,26 @@ if (!$erreur)
         $erreur = ($compte_sitting === false || $compte_sitting != $compt_cod);
     }
 }
-if ($erreur)
-{
+if ($erreur) {
     $titre_page   = 'Erreur';
     $contenu_page = 'Erreur d’authentification !';
 }
 
-if (!$erreur && empty($_REQUEST['visu_msg']))    // Liste des messages
-{
+if (!$erreur && empty($_REQUEST['visu_msg'])) {    // Liste des messages
     $titre_page   = 'Voir les 10 derniers messages';
     $contenu_page =
         '<table><tr><th class="titre">Date</th><th class="titre">Expediteur</th><th class="titre">Titre</th></tr>';
 
-    $req_msg = "select msg_cod, msg_titre, perso_nom, perso_cod, dmsg_lu, 
+    $req_msg = "select msg_cod, msg_titre, perso_nom, perso_cod, dmsg_lu,
 			to_char(msg_date2, 'DD/MM/YYYY hh24:mi:ss') as msg_date from messages
 		inner join messages_exp on emsg_msg_cod = msg_cod
 		inner join messages_dest on dmsg_msg_cod = msg_cod
 		inner join perso on perso_cod = emsg_perso_cod
-		where dmsg_perso_cod = $visu_perso
+		where dmsg_perso_cod = :perso
 		order by msg_date2 desc limit 10;";
     $stmt = $pdo->prepare($req_msg);
-    $stmt = $pdo->execute(array(":perso" =>  $_REQUEST['visu_perso']),$stmt);
-    while ($result = $stmt->fetch())
-    {
+    $stmt = $pdo->execute(array(":perso" =>  $_REQUEST['visu_perso']), $stmt);
+    while ($result = $stmt->fetch()) {
         $msg_cod        = $result['msg_cod'];
         $expediteur     = $result['perso_nom'];
         $expediteur_cod = $result['perso_cod'];
@@ -73,21 +67,17 @@ if (!$erreur && empty($_REQUEST['visu_msg']))    // Liste des messages
     $contenu_page .= "</table>";
 }
 
-if (!$erreur && !empty($_REQUEST['visu_msg']))    // Lecture d’un message
-{
-
-
-    $req_msg_ok = "select 1 from messages_dest where dmsg_perso_cod = $visu_perso AND dmsg_msg_cod = $visu_msg";
+if (!$erreur && !empty($_REQUEST['visu_msg'])) {    // Lecture d’un message
+    $req_msg_ok = "select 1 from messages_dest where dmsg_perso_cod = :perso AND dmsg_msg_cod = :msg";
     $stmt = $pdo->prepare($req_msg_ok);
-    $stmt = $pdo->execute(array(":perso" => $_REQUEST['visu_perso']),$stmt);
-    if (!$result = $stmt->fetch())
-    {
+    $stmt = $pdo->execute(array(":perso" => $_REQUEST['visu_perso'],
+    ":msg" =>$_REQUEST['visu_msg'] ), $stmt);
+    if (!$result = $stmt->fetch()) {
         $erreur       = true;
         $titre_page   = 'Erreur';
         $contenu_page = 'Message introuvable !';
-    } else
-    {
-        $req_msg = "select msg_cod, msg_titre, e.perso_nom, e.perso_cod, msg_corps, 
+    } else {
+        $req_msg = "select msg_cod, msg_titre, e.perso_nom, e.perso_cod, msg_corps,
 				to_char(msg_date2, 'DD/MM/YYYY hh24:mi:ss') as msg_date,
 				string_agg(d.perso_nom, ', ') as destinataires
 			from messages
@@ -98,9 +88,8 @@ if (!$erreur && !empty($_REQUEST['visu_msg']))    // Lecture d’un message
 			where msg_cod = :msg
 			group by msg_cod, msg_titre, e.perso_nom, e.perso_cod, msg_corps, msg_date2";
         $stmt = $pdo->prepare($req_msg);
-        $stmt = $pdo->execute(array(":msg" => $_REQUEST['visu_msg']),$stmt);
-        if ($result = $stmt->fetch())
-        {
+        $stmt = $pdo->execute(array(":msg" => $_REQUEST['visu_msg']), $stmt);
+        if ($result = $stmt->fetch()) {
             $msg_cod        = $result['msg_cod'];
             $expediteur     = $result['perso_nom'];
             $expediteur_cod = $result['perso_cod'];
@@ -121,7 +110,7 @@ if (!$erreur && !empty($_REQUEST['visu_msg']))    // Lecture d’un message
                 "update messages_dest set dmsg_lu = 'O' where dmsg_msg_cod = :msg and dmsg_perso_cod = :perso and dmsg_lu <> 'O'";
             $stmt = $pdo->prepare($req_lu);
             $stmt = $pdo->execute(array(":msg" => $_REQUEST['visu_msg'],
-                                      ":perso" => $_REQUEST['visu_perso']),$stmt);
+                                      ":perso" => $_REQUEST['visu_perso']), $stmt);
         }
         $contenu_page .= "</table>";
     }
