0a8bb2c3ab0fd63e53775dbfd25d92ea3c6593a5
Stephane DEWITTE <stephane.dewitte@gmail.com>
2020-02-05 13:57:52 +0100

    bugfix visu der evts

--- web/www/visu_derniers_evt.php
+++ web/www/visu_derniers_evt.php
@@ -22,55 +22,45 @@ $pdo = new bddpdo();
     <?php
     $erreur = 0;
     if (((isset($_REQUEST['visu_perso']) && $_REQUEST['visu_perso'] != '' &&
-          isset($compt_cod) && $compt_cod != '')) && !isset($_REQUEST['voir_tous']))
-    {
-
-        $req  = "select pcompt_compt_cod 
-            from perso_compte 
+          isset($compt_cod) && $compt_cod != '')) && !isset($_REQUEST['voir_tous'])) {
+        $req  = "select pcompt_compt_cod
+            from perso_compte
             where pcompt_perso_cod = :perso
-            or pcompt_perso_cod = 
+            or pcompt_perso_cod =
                   ( select pfam_perso_cod from perso_familier where pfam_familier_cod = :perso  )";
         $stmt = $pdo->prepare($req);
         $stmt = $pdo->execute(array(
                                   ":perso" => $_REQUEST['visu_perso']
                               ), $stmt);
 
-        if (!$result = $stmt->fetch())    // Monstre
-        {
+        if (!$result = $stmt->fetch()) {    // Monstre
             echo "<p>Erreur ! Vous ne pouvez pas consulter les événements de ce perso !</p>";
             $erreur = 1;
-        } else if ($result['pcompt_compt_cod'] != $compt_cod)  // Sitté ?
-        {
+        } elseif ($result['pcompt_compt_cod'] != $compt_cod) {  // Sitté ?
             $sitte = $result['pcompt_compt_cod'];
             // Test sitting.
-            $req  = 'select csit_compte_sitteur from compte_sitting where 
-                csit_compte_sitteur = :compte and 
-                csit_compte_sitte = :sitte and csit_ddeb < now() and 
+            $req  = 'select csit_compte_sitteur from compte_sitting where
+                csit_compte_sitteur = :compte and
+                csit_compte_sitte = :sitte and csit_ddeb < now() and
                 csit_dfin > now()';
             $stmt = $pdo->prepare($req);
             $stmt = $pdo->execute(array(":compte" => $compt_cod,
                                         ":sitte"  => $sitte), $stmt);
 
-            if (!$stmt->fetch())  // Pas un cas de sitting.
-            {
+            if (!$stmt->fetch()) {  // Pas un cas de sitting.
                 echo "<p>Erreur ! Vous ne pouvez pas consulter les événements de ce perso !</p>";
                 $erreur = 1;
             }
         }
-    } else if (isset($compt_cod) && $compt_cod != '' && isset($_REQUEST['voir_tous']) && $_REQUEST['voir_tous'] == 1)
-    {
-    } else    // Missing info
-    {
+    } elseif (isset($compt_cod) && $compt_cod != '' && isset($_REQUEST['voir_tous']) && $_REQUEST['voir_tous'] == 1) {
+    } else {    // Missing info
         $erreur = 1;
     }
 
-    if ($erreur == 0)
-    {
-
+    if ($erreur == 0) {
         $tableau_numeros = array();
         $tableau_noms    = array();
-        if (isset($voir_tous) && $voir_tous == 1)
-        {
+        if (isset($voir_tous) && $voir_tous == 1) {
             $req_persos = "select perso_cod, perso_nom
                 from perso
                 inner join perso_compte on pcompt_perso_cod = perso_cod
@@ -89,37 +79,31 @@ $pdo = new bddpdo();
             $stmt       = $pdo->prepare($req_persos);
             $stmt       = $pdo->execute(array(":compte" => $compt_cod), $stmt);
 
-            while ($result = $stmt->fetch())
-            {
+            while ($result = $stmt->fetch()) {
                 $tableau_numeros[] = $result['perso_cod'];
                 $tableau_noms[]    = $result['perso_nom'];
             }
-        } else
-        {
+        } else {
             $tableau_numeros[] = $visu_perso;
         }
 
 
         $premier_perso = true;
 
-        foreach ($tableau_numeros as $key => $numero_perso)
-        {
-            if (!$premier_perso)
-            {
+        foreach ($tableau_numeros as $key => $numero_perso) {
+            if (!$premier_perso) {
                 echo '<hr />';
             }
 
 
-            if (isset($tableau_noms[$key]))
-            {
+            if (isset($tableau_noms[$key])) {
                 echo "<p><strong>Pour " . $tableau_noms[$key] . " :</strong></p>";
             }
             $levt   = new ligne_evt();
             $allevt = $levt->getByPersoNonLu($numero_perso);
 
 
-            if (count($allevt) != 0)
-            {
+            if (count($allevt) != 0) {
                 ?>
                 <table>
 
@@ -128,18 +112,19 @@ $pdo = new bddpdo();
                             <p>
                                 <?php
                                 $allevtformat = $levt->mise_en_page_evt($allevt, true, true, false);
-                                foreach ($allevt as $detailevt)
-                                {
-                                    printf("%s : $texte_evt (%s).</br>", format_date($detailevt->levt_date),
-                                           $detailevt->levt_texte);
-                                }
-                                ?>
+
+                foreach ($allevt as $detailevt) {
+                    printf(
+                        "%s : $texte_evt (%s).</br>",
+                        format_date($detailevt->levt_date),
+                        $detailevt->levt_texte
+                    );
+                } ?>
                         </td>
                     </tr>
                 </table>
                 <?php
-            } else
-            {
+            } else {
                 echo "<p>Pas d’événements depuis votre dernière DLT</p>";
             }
         }
