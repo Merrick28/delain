a0c57f8c0e54091fbc634c09fb5bcef71f9353ed
Merrick28 <stephane.dewitte@gmail.com>
2020-02-10 07:39:06 +0100

    passage en pdo

--- shell/passage_pdo.py
+++ shell/passage_pdo.py
@@ -6,5 +6,10 @@ fw= open("../web/www/jeu_test/admin_factions_missions.tmp","w+")
 with open ('../web/www/jeu_test/admin_factions_missions.php', 'r' ) as f:
     content = f.read()
 content_new = re.sub("\$db->query\(\$([a-zA-Z_]*)\)", r'$stmt = $pdo->query($\1)', content, flags = re.M)
+content_new = content_new.replace("while($db->next_record())","while($result = $stmt->fetch())")
+content_new = content_new.replace("if ($db->next_record())","if($result = $stmt->fetch())")
+content_new = content_new.replace("$db->next_record()","$result = $stmt->fetch()")
+content_new = re.sub("\$db->f\('([a-zA-Z_]*)'\)", r"$result['\1']", content_new, flags = re.M)
+content_new = re.sub("\$db->f\(\"([a-zA-Z_]*)\"\)", r"$result['\1']", content_new, flags = re.M)
 fw.write(content_new)
 fw.close
