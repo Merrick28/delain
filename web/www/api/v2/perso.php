9a0648f9a48ffbb53067690a0eccfe2fc47f14ea
Merrick28 <stephane.dewitte@gmail.com>
2020-02-17 07:32:05 +0100

    bugfix api perso

--- web/www/api/v2/perso.php
+++ web/www/api/v2/perso.php
@@ -93,7 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
     // Recherche du type de perso en cours de création
     $compte = new compte();
-    $compte = $verif_connexion->compte;
+    $compte->charge($compt_cod);
 
     $nb_perso           = $compte->compte_nombre_perso();
     $possede_4e         = $compte->possede_4e_perso();
