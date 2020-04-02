595c3933608c5c2112786fa5b1335ba34e223900
Merrick28 <stephane.dewitte@gmail.com>
2020-04-02 14:39:50 +0200

    [ci skip] Script remplissage ok

--- shell/init_distance_etage.sh
+++ shell/init_distance_etage.sh
@@ -4,15 +4,19 @@ source $(dirname $0)/env
 #IFS=$'\n'
 START=0
 for i in $(cat ./liste_etage); do
+  echo "################"
+  echo "Etage = " $i
   while [[ $TEMPRESULT != "termine" ]]; do
+    echo "Start = " $START
     TEMPRESULT=$(
       $psql -A -q -t -d delain -U ${USERNAME} <<EOF
 select remplissage_table_distance_etage_offset($i,$START);
 EOF
     )
-    echo "Etage = " $i
-    echo "Resultat = " $TEMPRESULT
-    echo "Start = " $START
+
+    echo "Resultat = " $TEMPRESULT " sur etage " $i
+    echo "--"
+
     START=$((START + 50))
   done
   TEMPRESULT=encore
