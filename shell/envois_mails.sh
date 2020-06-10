d8384e2a8c3ef00e9aa760390525ce516084e88e
Merrick28 <stephane.dewitte@gmail.com>
2020-06-10 15:41:11 +0200

    Bugfix logs envois mails

--- shell/envois_mails.sh
+++ shell/envois_mails.sh
@@ -2,4 +2,4 @@
 # TODO : commenter chaque ligne pour expliquer le but
 # test de commit
 source `dirname $0`/env
-/usr/bin/php $webroot/envois_mail.php > $logdir/envoi_mail.log
+/usr/bin/php $webroot/envois_mail.php >> $logdir/envoi_mail.log
