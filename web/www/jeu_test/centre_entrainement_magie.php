3c4f4b3f6812ab0522f29501656ab2973c8b9186
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2020-10-22 14:46:45 +0200

    rationalisation test appel de page

--- web/www/jeu_test/centre_entrainement_magie.php
+++ web/www/jeu_test/centre_entrainement_magie.php
@@ -2,8 +2,8 @@
 
 
 $verif_connexion = new verif_connexion();
-$verif_connexion::verif_appel();
-$verif_connexion->verif();
+
+$verif_connexion->verif(true);
 $perso_cod = $verif_connexion->perso_cod;
 $compt_cod = $verif_connexion->compt_cod;
 
