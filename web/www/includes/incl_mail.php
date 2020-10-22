5a9d6e978440cdac1af5183c21fd55898204e55c
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2020-10-22 11:43:04 +0200

    Security fix

--- web/www/includes/incl_mail.php
+++ web/www/includes/incl_mail.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* 
 Originally 
 By: Jon S. Stevens jon@clearink.com 
@@ -29,7 +29,7 @@ Organization : Techno-Prime, France
                if a MX tell us that a user is not known, perhaps can we trust it. 
                Also added a \r to all fputs because some servers doesnt understand \n. 
                However, there are a few servers correctly configured on Earth... :-) 
-*/ 
+*/
 
 /*  This function takes in an email address (say 'shane@tuna.org') 
 *  and tests to see if it's a valid email address. 
@@ -54,118 +54,99 @@ Organization : Techno-Prime, France
 *                    with the MX server hostname 
 * 
 *  EXAMPLE code for use is at the very end of this function. 
-*/ 
+*/
 
-function validateEmail ($email) 
-{ 
-    $debug_=0; 
+function validateEmail($email)
+{
+    $debug_ = 0;
     // used for SMTP HELO argument 
-    global $SERVER_NAME;   
+    global $SERVER_NAME;
 
     // initialize our return array, populating with default values 
-    $return=array(false,"","",""); 
+    $return = array(false, "", "", "");
 
     // assign our user part and domain parts respectively to seperate              
     // variables 
-    $donnees = explode("@",$email);
+    $donnees = explode("@", $email);
     if (count($donnees) != 2)
     {
-        $return[0] = false; 
-        $return[1] = "Invalid email address (bad format)"; 
-        $return[2] = false; 
-        
+        $return[0] = false;
+        $return[1] = "Invalid email address (bad format)";
+        $return[2] = false;
+
         return $return;
     }
-    
-    list($user, $domain) = $donnees; 
 
-    /*if($debug_==1) {
-        echo"user: $user<BR>";    
-        echo"domain: $domain<BR>";    
-    } */
+    list($user, $domain) = $donnees;
+
+
     // split up the domain name into sub-parts 
-    $arr=explode(".",$domain); 
+    $arr = explode(".", $domain);
 
     // figure out how many parts to the host/domain name portion there are 
-    $count=count($arr); 
+    $count = count($arr);
 
     // get our Top-Level Domain portion (i.e. foobar.org) 
-    $tld=$arr[$count-2].".".$arr[$count-1]; 
+    $tld = $arr[$count - 2] . "." . $arr[$count - 1];
 
     // check that an MX record exists for Top-Level Domain, and if so 
     // start our email address checking 
-    if (checkdnsrr($domain,"MX")) 
-    { 
-         if($debug_==1) { 
-          echo"Check DNS RR OK<BR>"; 
-         } 
+    if (checkdnsrr($domain, "MX"))
+    {
+
         // Okay...valid dns reverse record; test that MX record for 
         // host exists, and then fill the 'mxhosts' and 'weight' 
         // arrays with the correct information 
         // 
-        if (getmxrr($domain,$mxhosts,$weight)) 
+        if (getmxrr($domain, $mxhosts, $weight))
         {
-            /*if($debug_==1) {
-                echo "MX LOOKUP RESULTS :<BR>"; 
-                for ( $i = 0; $i < count($mxhosts); $i++ ) { 
-                    echo "??????o $weight[$i] $mxhosts[$i]<BR>"; 
-                } 
-                echo "<strong>".count($mxhosts)." mail-servers found for this domain</strong><BR>"; 
 
-            } */
 
-        // sift through the 'mxhosts' connecting to each host 
-            for ( $i=0; $i < count($mxhosts); $i++) 
-            { 
+            // sift through the 'mxhosts' connecting to each host
+            for ($i = 0; $i < count($mxhosts); $i++)
+            {
                 // open socket on port 25 to mxhosts, setting                          
                 // returned file pointer to the variable 'fp' 
-                $fp = fsockopen ($mxhosts[$i], 25 ); 
+                $fp = fsockopen($mxhosts[$i], 25);
 
                 // if the 'fp' was set, then goto work 
-                if ($fp) 
+                if ($fp)
                 {
-                    /*if($debug_==1) {
-                    echo"<BR><BR><h2>$mxhosts[$i]</h2>"; 
-                    echo"Socket Opened successfully...<BR>"; 
-                       }*/
-                // work variables 
-                    $s = 0; 
-                    $c = 0; 
-                    $out = ""; 
+
+                    // work variables
+                    $s   = 0;
+                    $c   = 0;
+                    $out = "";
                     // set our created socket for 'fp' to      
                     // non-blocking mode 
                     // so our fgets() calls will return 
                     // right away 
-                    stream_set_blocking ( $fp, false ); 
+                    stream_set_blocking($fp, false);
 
                     // as long as our 'out' variable has a 
                     // null value ("") 
                     // keep looping (do) until we get 
                     // something 
                     // 
-                    do 
-                    { 
+                    do
+                    {
                         // output of the stream assigned 
                         // to 'out' variable 
-                        $out = fgets ( $fp, 2500 );
-                        /*if($debug_==1) {
-                           if($out != "")  echo"out: $out<BR>"; 
-                        }*/
+                        $out = fgets($fp, 2500);
+
                         // if we get an "220" code (service ready code (i.e greeting)) 
                         // increment our work (code (c)) variable, and null 
                         // out our output variable for a later loop test 
                         // 
-                        if ( preg_match  ( "/^220/", $out ) )
+                        if (preg_match("/^220/", $out))
                         {
-                            /*if($debug_==1) {
-                            echo"Service ready on recipient machine.<BR>"; 
-                            }*/
-                            $s = 0; 
-                            $out = ""; 
-                            $c++; 
-                            $return[2] = true; 
-                            $return[3] = $mxhosts[$i]; 
-                        } 
+
+                            $s   = 0;
+                            $out = "";
+                            $c++;
+                            $return[2] = true;
+                            $return[3] = $mxhosts[$i];
+                        }
                         // elseif c is greater than 0 
                         // and 'out' is null (""), 
                         // we got a code back from some 
@@ -173,128 +154,110 @@ function validateEmail ($email)
                         // through this loop at least 
                         // once 
                         // 
-                        else if (($c > 0) && ($out == "")) 
-                        { 
-                               $return[2] = true; 
-                            break; 
-                        } 
+                        else if (($c > 0) && ($out == ""))
+                        {
+                            $return[2] = true;
+                            break;
+                        }
 
                         // else increment our 's' 
                         // counter 
-                        else 
-                        { $s++;    } 
-                     
+                        else
+                        {
+                            $s++;
+                        }
+
                         // and if 's' is 9999, break, to 
                         // keep from looping 
                         // infinetly 
-                        if ( $s == 9999 ) {
-                            /*if($debug_==1) {
-                                echo"Reached maximum 10000 loops, breaking.<BR>"; 
-                            } */
-                        break; 
-                        } 
-                     
-                    } while ( $out == "" ); 
+                        if ($s == 9999)
+                        {
+
+                            break;
+                        }
+
+                    } while ($out == "");
 
                     // reset our file pointer to blocking 
                     // mode, so we wait 
                     // for communication to finish before 
                     // moving on... 
-                    stream_set_blocking ( $fp, true ); 
+                    stream_set_blocking($fp, true);
 
                     // talk to the MX mail server, 
                     // validating ourself (HELO) 
-                    fputs ( $fp, "HELO ".$SERVER_NAME."\r\n");
-                    /*if($debug_==1) {
-                       echo"<BR>HELO $SERVER_NAME<BR>";
-                   } */
+                    fputs($fp, "HELO " . $SERVER_NAME . "\r\n");
+
                     // get the mail servers reply, assign to 
                     // 'output' (ignored) 
-                    $output = fgets ( $fp, 2000 );
-                    /* if($debug_==1) {
-                       echo"output : $output<BR>";
-                    } */
-                      // give a bogus "MAIL FROM:" header to 
+                    $output = fgets($fp, 2000);
+
+                    // give a bogus "MAIL FROM:" header to
                     // the server 
-                    fputs ($fp,"MAIL FROM: <info@".$domain.">\r\n");
-                    /*if($debug_==1) {
-                       echo"MAIL FROM: <info@".$domain."><BR>";
-                   } */
+                    fputs($fp, "MAIL FROM: <info@" . $domain . ">\r\n");
+
                     // get output again (ignored) 
-                    $output = fgets ( $fp, 2000 );
-                    /*if($debug_==1) {
-                      echo"output : $output<BR>";
-                   } */
+                    $output = fgets($fp, 2000);
+
                     // give RCPT TO: header for the email 
                     // address we are testing 
-                    fputs($fp,"RCPT TO: <".$email.">\r\n");
-                    /*if($debug_==1) {
-                        echo"RCPT TO: <$email><BR>"; 
-                    } */
+                    fputs($fp, "RCPT TO: <" . $email . ">\r\n");
+
                     // get final output for validity testing 
                     // (used) 
-                    $output = fgets ($fp, 2000);
-                    /*if($debug_==1) {
-                      echo"output : $output<BR>";
-                   } */
+                    $output = fgets($fp, 2000);
+
                     // test the reply code from the mail 
                     // server for the 550 (no recipient) code 
-                    if (preg_match ("/^550/",$output))
+                    if (preg_match("/^550/", $output))
                     {
-                        /*if($debug_==1) {
-                            echo"Recipient doesnt exist<BR>";
-                          } */
-                    // set our true/false(ness) 
-                    // array item for testing 
-                        $return[0] = false; 
-                        $return[1] = $output; 
 
-                    } 
-                    else 
-                    { 
+                        // set our true/false(ness)
+                        // array item for testing
+                        $return[0] = false;
+                        $return[1] = $output;
+
+                    } else
+                    {
                         // otherwise, the address is valid, 
                         // fillin the 2nd array item 
                         // with the mail servers reply 
                         // code for user to test if they 
                         // want 
-                        $return[0] = true; 
+                        $return[0] = true;
                         $return[1] = $output;
-                        /* if($debug_==1) {
-                            echo"The recipient exists <BR>";
-                         } */
-                    } 
-                 
+
+                    }
+
                     // tell the mail server we are done 
                     // talking to it 
-                    fputs ( $fp, "QUIT\r\n");
-                    /*if($debug_==1) {
-                       echo"Quit";
-                   } */
+                    fputs($fp, "QUIT\r\n");
+    
                     // close the file pointer 
-                    fclose($fp); 
+                    fclose($fp);
 
                     // if we got a good value break, 
                     // otherwise, we'll keep 
                     // trying MX records until we get a good 
                     // value, or we 
                     // exhaust our possible MX servers 
-                    if ($return[0] == false) {
-                        /* if($debug_==1) {
-                             echo"Recipient doesnt exist... Breaking";
-                         }    */
-                    break; 
-                     } 
-                } 
-            } 
-        } 
-    } else { 
-    // No MX record appears for the specified Top-Level Domain; possibly 
-    // an invalid host/domain name was specified. 
-        $return[0] = false; 
-        $return[1] = "Invalid email address (bad domain name)"; 
-        $return[2] = false; 
+                    if ($return[0] == false)
+                    {
+
+                        break;
+                    }
+                }
+            }
+        }
+    } else
+    {
+        // No MX record appears for the specified Top-Level Domain; possibly
+        // an invalid host/domain name was specified.
+        $return[0] = false;
+        $return[1] = "Invalid email address (bad domain name)";
+        $return[2] = false;
     } // end if checkdnsrr() 
 
     // return the array for the user to test against 
-    return $return; 
+    return $return;
 }
