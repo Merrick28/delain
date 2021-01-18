21b3c1c8fa2dbd5dc65866a25b56ac31825e5def
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2021-01-18 16:33:06 +0100

    Changement mode livraison

--- shell/scan_diff.sh
+++ shell/scan_diff.sh
@@ -2,7 +2,7 @@
 source $(dirname $0)/env
 CURRENT_SCRIPT=$(readlink -f "$0")
 export SCRIPTPATH=$(dirname "$CURRENT_SCRIPT")
-cd ${CURRENT_SCRIPT}/../
+cd ${SCRIPTPATH}/../
 git diff --name-only origin/master | grep ^fonctions_sql/ | sort >.diff_to_apply
 echo "Modifs à appliquer " >>${shellroot}/livraison.log
 cat .diff_to_apply >>${shellroot}/livraison.log
