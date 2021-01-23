6e5acf85abe9abd27cec39787eff6e6bb5cc3a4f
Merrick <stephane.dewitte@gmail.com>
2021-01-23 07:27:32 +0100

    test affichage

--- shell/scan_diff.sh
+++ shell/scan_diff.sh
@@ -4,5 +4,6 @@ CURRENT_SCRIPT=$(readlink -f "$0")
 export SCRIPTPATH=$(dirname "$CURRENT_SCRIPT")
 cd ${SCRIPTPATH}/../
 git diff --name-only origin/master | grep ^fonctions_sql/ | sort >.diff_to_apply
+git diff --name-only origin/master | grep ^fonctions_sql/ | sort >> ${shellroot}/livraison.log
 echo "Modifs à appliquer " >>${shellroot}/livraison.log
 cat .diff_to_apply >>${shellroot}/livraison.log
