636a6c7414a11cd2023f50701248e21afdb0c197
Marlyza <maria.lyzzie@gmail.com>
2021-03-18 15:28:19 +0100

    Ajout ligne event sur lance sort bonus

--- fonctions_sql/sort_lance_bonus.sql
+++ fonctions_sql/sort_lance_bonus.sql
@@ -256,6 +256,17 @@ begin
       else
         code_retour := code_retour||'vous lui donnez un bonus  `'|| nom_bonus || '` de valeur '|| v_bonmal_valeur::text || ' pendant '|| v_bonmal_duree::text || ' tour(s).<br>';
       end if;
+
+      texte_evt := '[attaquant] a lancé ' || nom_sort || ' sur [cible].';
+      insert into ligne_evt(levt_cod, levt_tevt_cod, levt_date, levt_type_per1, levt_perso_cod1, levt_texte, levt_lu,
+                            levt_visible, levt_attaquant, levt_cible)
+      values (nextval('seq_levt_cod'), 14, now(), 1, lanceur, texte_evt, 'O', 'O', lanceur, cible);
+      if (lanceur != cible) then
+          insert into ligne_evt(levt_cod, levt_tevt_cod, levt_date, levt_type_per1, levt_perso_cod1, levt_texte, levt_lu,
+                                levt_visible, levt_attaquant, levt_cible)
+          values (nextval('seq_levt_cod'), 14, now(), 1, cible, texte_evt, 'N', 'O', lanceur, cible);
+      end if;
+
   end if;
 
 -------------------------
