eea95aeab004354e7cc2eb58d974e3495e718ada
Marlyza <maria.lyzzie@gmail.com>
2021-04-26 17:25:59 +0200

    Monture2.0: interface de saisie des ordres monture (ajout d'ordre)

--- fonctions_sql/monture_ordonner.sql
+++ fonctions_sql/monture_ordonner.sql
@@ -15,7 +15,12 @@ AS $_$declare
   v_monture_nom text;
   temp_competence text;   -- text du jet de compétence
   des integer ;     -- jet de dé (incident facheux)
-
+  dir_x integer ;     -- ordre: direction en x
+  dir_y integer ;     --  ordre: direction en y
+  dist integer ;     --  ordre: distance
+  v_param_ia json ;     --  ordre: distance
+  v_nb_ordre integer ;     --  nombre d'ordre déjà donné
+  v_num_ordre integer ;     --  N° d'ordre actuel
 
 begin
 	code_retour := '';
@@ -29,17 +34,16 @@ begin
     return '<p>Erreur ! Vous n''avez pas suffisament de PA !';
   end if;
 
-  select perso_nom into v_monture_nom from perso where perso_cod=v_monture ;
+  select perso_nom, COALESCE((perso_misc_param->>'ia_monture_ordre')::json, '[]'::json) into v_monture_nom, v_param_ia from perso where perso_cod=v_monture ;
   if not found then
     return '<p>Erreur ! la monture n''a pas été trouvée !';
   end if;
 
+	select count(*), coalesce(max(f_to_numeric(value->>'ordre')) , 0) into v_nb_ordre, v_num_ordre from json_array_elements (v_param_ia);
 
-  -- code_retour := code_retour|| ' dir=' || ( COALESCE( (((v_param)->>'dir')::text) , '1:2')::text) ;
-  -- code_retour := code_retour|| ' dist=' || ( COALESCE( (((v_param)->>'dist')::text)::numeric , 0)::text) ;
 
   -- Test de compétence équitation (difficulté 0) => gère le la consommation de PA
-  temp_competence := monture_competence(v_perso, 3, v_monture, 0);
+  temp_competence := monture_competence(v_perso, 3, v_monture, v_nb_ordre);
   code_retour := code_retour||split_part(temp_competence,';',3);
 
   -- Test sur le jet de compétence
@@ -48,6 +52,16 @@ begin
 
       -- evenement déchevaucher (107)
       perform insere_evenement(v_perso, v_monture, 107, '[attaquant] a donné un ordre à sa monture [cible].', 'O', NULL);
+
+      -- réaliser l'action !!!
+      v_num_ordre := v_num_ordre + 1 ;
+      dist :=  f_to_numeric(v_param->>'dist') ;     --  ordre: distance
+      dir_x :=  f_to_numeric(v_param->>'dir_x') ;     --  ordre: distance
+      dir_y :=  f_to_numeric(v_param->>'dir_y') ;     --  ordre: distance
+
+      update perso
+        set perso_misc_param = COALESCE(perso_misc_param::jsonb, '{}'::jsonb) || (json_build_object( 'ia_monture_ordre' , ((v_param_ia::jsonb) || (json_build_object( 'ordre' , v_num_ordre, 'dir_x' , dir_x, 'dir_y' , dir_y , 'dist' , dist )::jsonb)))::jsonb)
+        where perso_cod=v_monture ;
 
   else
       -- si echec du jet de compétence
--- web/www/jeu_test/monture_ordre.php
+++ web/www/jeu_test/monture_ordre.php
@@ -5,6 +5,16 @@ $verif_connexion->verif();
 $perso_cod = $verif_connexion->perso_cod;
 $compt_cod = $verif_connexion->compt_cod;
 
+$arr_img = [
+    "1:0"=> "N.png",
+    "-1:0"=> "S.png",
+    "0:1"=> "E.png",
+    "0:-1"=> "W.png",
+    "1:1"=> "NE.png",
+    "1:-1"=> "NW.png",
+    "-1:1"=> "SE.png",
+    "-1:-1"=> "SW.png",
+];
 
 $contenu_page = '';
 
@@ -24,27 +34,66 @@ if ($perso->perso_type_perso == 3){
     $contenu_page .= "<br><p>Vous chevaucher actuellement: <a href=\"visu_desc_perso.php?visu=".$monture->perso_cod."\">".$monture->perso_nom."</a></p>";
 
     // traitment des nouveau ordres:
-    if (isset($_REQUEST["NOUVEL_ORDRE"]))
+    if (isset($_REQUEST["ORDRE_ADD"]))
     {
         $contenu_page .= "<hr><b>ACTION</b>: Donner un ordre à la monture<br>";
         // check validité :
         $msg = "";
         if ( !isset($_REQUEST["distance"]) || (int)$_REQUEST["distance"]<=0 ) $msg .= "La distance de l'ordre n'est pas valide! ";
         if ( !isset($_REQUEST["direction"]) ) $msg .= "Vous n'avez pas sélectionné de direction! ";
+        $dir=explode(":", $_REQUEST["direction"]);
+        $dir_x = (int)$dir[0];
+        $dir_y = (int)$dir[1];
+        if ( $dir_x <-1 || $dir_x >1 || $dir_y <-1 || $dir_y >1  || ($dir_y==0 && $dir_x==0)) $msg .= "Vous avez donné un mauvaise ordre de direction! ";
         if ($msg != "")
         {
             $contenu_page .= $msg."<br>L'ordre n'est <b>pas valide</b>, les PA n'ont pas été depensés!<br>";
         }
         else
         {
-            $contenu_page .= $perso->monture_ordre( "ADD", [ "dir" => $_REQUEST["direction"], "dist" => $_REQUEST["distance"] ] );
+            $contenu_page .= $perso->monture_ordre( "ADD", [ "dir_x" => $dir_x, "dir_y" => $dir_y, "dist" => (int)$_REQUEST["distance"] ] );
+            $monture->charge( $perso->perso_monture ); // recharger le perso montures (avec les nouveaux ordres)
         }
 
         //print_r($_REQUEST);die();
         $contenu_page .= "<hr>";
     }
 
+    // traitment des nouveau ordres:
+    if (isset($_REQUEST["ORDRE_DEL"]))
+    {
+        $contenu_page .= "<hr><b>ACTION</b>: Supprimer un ordre de la monture<br>";
+        // check validité :
+        $msg = "";
+
+        $contenu_page .= print_r($_REQUEST, true);
+
+        //print_r($_REQUEST);die();
+        $contenu_page .= "<hr>";
+    }
+
     $contenu_page .= "<br><b><u>Liste des ordres actifs</u></b>: <br>";
+    $ordres = json_decode($monture->perso_misc_param) ;
+    if (sizeof($ordres->ia_monture_ordre) >0)
+    {
+        $contenu_page .= '<form name="monture_dep" id="monture_dep" method="post" action="monture_ordre.php">';
+        $contenu_page .= '<input type="hidden" name="num_ordre" id="num_ordre" value="">';
+        $a_ordres = [] ;
+        foreach ($ordres->ia_monture_ordre as $k => $o)
+        {
+            $a_ordres[$o->ordre] = $k ;
+        }
+        foreach ($a_ordres as $k)
+        {
+            $o = $ordres->ia_monture_ordre[$k] ;
+            $img = "<img style='margin:3px; vertical-align: middle;' src='/images/interface/".$arr_img[$o->dir_y.":".$o->dir_x]."'>";
+            $contenu_page .=  "<br><span><input onclick=\"$('#num_ordre').val(".($o->ordre).");\" name=\"ORDRE_DEL\" type=\"submit\" value=\"Supprimer (2 PA)\"  class=\"test\">&nbsp;&nbsp;&nbsp;&nbsp;N° {$o->ordre} : ";
+            for($i=0; $i<$o->dist; $i++) $contenu_page .= $img;
+            $contenu_page .= "&nbsp;&nbsp;</span>"; ;
+        }
+        $contenu_page .= '</form>';
+    }
+
 
     $contenu_page .= "<br><b><u>Donner un nouvel ordre</u></b>: <br>Sélectionner la direction et la distance à parcourir:<br><br>";
 
@@ -54,7 +103,7 @@ if ($perso->perso_type_perso == 3){
     //$contenu_page .= '&nbsp;&nbsp;Distance:&nbsp;<input name="monture_dist" size="4" value="1" >';
     //$contenu_page .= '&nbsp;&nbsp;<input type="submit" value="Ordoner (2 PA)"  class="test">';
 
-    $contenu_page .= '<form name="monture_dep" id="monture_dep" method="post" action="monture_ordre.php"><table>';
+    $contenu_page .= '<form name="monture_order" id="monture_order" method="post" action="monture_ordre.php"><table>';
     for ($l=1; $l<=3; $l++) {
         $contenu_page .= '<tr>';
         for ($c=1; $c<=3; $c++) {
@@ -66,7 +115,7 @@ if ($perso->perso_type_perso == 3){
             }
             $contenu_page .= '</td>';
         }
-        if ($l==1 && $c>3) $contenu_page .= '<td rowspan="3" class="soustitre2" style="text-align: center;">&nbsp;&nbsp;<input name="NOUVEL_ORDRE" type="submit" value="Ordoner (2 PA)"  class="test">&nbsp;&nbsp;</td>';
+        if ($l==1 && $c>3) $contenu_page .= '<td rowspan="3" class="soustitre2" style="text-align: center;">&nbsp;&nbsp;<input name="ORDRE_ADD" type="submit" value="Donner l\'ordre(2 PA)"  class="test">&nbsp;&nbsp;</td>';
         $contenu_page .= '</tr>';
     }
 
