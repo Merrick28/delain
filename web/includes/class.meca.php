ec07ed6b55a0055bf18be5857c25904c6a723eae
Marlyza <maria.lyzzie@gmail.com>
2021-07-08 17:26:18 +0200

    ea_mouv: interface admin creation des meca creer/edit/supprim

---
+++ web/includes/class.meca.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * includes/class.meca.php
+ */
+
+/**
+ * Class meca
+ *
+ * Gère les objets BDD de la table meca
+ */
+class meca
+{
+    var $meca_cod;
+    var $meca_nom;
+    var $meca_type = 'G';
+    var $meca_pos_etage;
+    var $meca_pos_type_aff;
+    var $meca_pos_decor;
+    var $meca_pos_decor_dessus;
+    var $meca_pos_passage_autorise;
+    var $meca_pos_modif_pa_dep;
+    var $meca_pos_ter_cod;
+    var $meca_mur_type;
+    var $meca_mur_tangible = NULL;
+    var $meca_mur_illusion = NULL;
+    var $meca_si_active;
+    var $meca_si_desactive;
+
+    function __construct()
+    {
+    }
+
+    /**
+     * Charge dans la classe un enregistrement de meca
+     * @global bdd_mysql $pdo
+     * @param integer $code => PK
+     * @return boolean => false si non trouvé
+     */
+    function charge($code)
+    {
+        $pdo = new bddpdo;
+        $req = "select * from meca where meca_cod = ?";
+        $stmt = $pdo->prepare($req);
+        $stmt = $pdo->execute(array($code),$stmt);
+        if(!$result = $stmt->fetch())
+        {
+            return false;
+        }
+        $this->meca_cod = $result['meca_cod'];
+        $this->meca_nom = $result['meca_nom'];
+        $this->meca_type = $result['meca_type'];
+        $this->meca_pos_etage = $result['meca_pos_etage'];
+        $this->meca_pos_type_aff = $result['meca_pos_type_aff'];
+        $this->meca_pos_decor = $result['meca_pos_decor'];
+        $this->meca_pos_decor_dessus = $result['meca_pos_decor_dessus'];
+        $this->meca_pos_passage_autorise = $result['meca_pos_passage_autorise'];
+        $this->meca_pos_modif_pa_dep = $result['meca_pos_modif_pa_dep'];
+        $this->meca_pos_ter_cod = $result['meca_pos_ter_cod'];
+        $this->meca_mur_type = $result['meca_mur_type'];
+        $this->meca_mur_tangible = $result['meca_mur_tangible'];
+        $this->meca_mur_illusion = $result['meca_mur_illusion'];
+        $this->meca_si_active = $result['meca_si_active'];
+        $this->meca_si_desactive = $result['meca_si_desactive'];
+        return true;
+    }
+
+    /**
+     * Stocke l'enregistrement courant dans la BDD
+     * @global bdd_mysql $pdo
+     * @param boolean $new => true si new enregistrement (insert), false si existant (update)
+     */
+    function stocke($new = false)
+    {
+        $pdo = new bddpdo;
+        if($new)
+        {
+            $req = "insert into meca (
+            meca_nom,
+            meca_type,
+            meca_pos_etage,
+            meca_pos_type_aff,
+            meca_pos_decor,
+            meca_pos_decor_dessus,
+            meca_pos_passage_autorise,
+            meca_pos_modif_pa_dep,
+            meca_pos_ter_cod,
+            meca_mur_type,
+            meca_mur_tangible,
+            meca_mur_illusion,
+            meca_si_active,
+            meca_si_desactive                        )
+                    values
+                    (
+                        :meca_nom,
+                        :meca_type,
+                        :meca_pos_etage,
+                        :meca_pos_type_aff,
+                        :meca_pos_decor,
+                        :meca_pos_decor_dessus,
+                        :meca_pos_passage_autorise,
+                        :meca_pos_modif_pa_dep,
+                        :meca_pos_ter_cod,
+                        :meca_mur_type,
+                        :meca_mur_tangible,
+                        :meca_mur_illusion,
+                        :meca_si_active,
+                        :meca_si_desactive                        )
+    returning meca_cod as id";
+            $stmt = $pdo->prepare($req);
+            $stmt = $pdo->execute(array(
+                ":meca_nom" => $this->meca_nom,
+                ":meca_type" => $this->meca_type,
+                ":meca_pos_etage" => $this->meca_pos_etage,
+                ":meca_pos_type_aff" => $this->meca_pos_type_aff,
+                ":meca_pos_decor" => $this->meca_pos_decor,
+                ":meca_pos_decor_dessus" => $this->meca_pos_decor_dessus,
+                ":meca_pos_passage_autorise" => $this->meca_pos_passage_autorise,
+                ":meca_pos_modif_pa_dep" => $this->meca_pos_modif_pa_dep,
+                ":meca_pos_ter_cod" => $this->meca_pos_ter_cod,
+                ":meca_mur_type" => $this->meca_mur_type,
+                ":meca_mur_tangible" => $this->meca_mur_tangible,
+                ":meca_mur_illusion" => $this->meca_mur_illusion,
+                ":meca_si_active" => $this->meca_si_active,
+                ":meca_si_desactive" => $this->meca_si_desactive,
+            ),$stmt);
+
+
+            $temp = $stmt->fetch();
+            $this->charge($temp['id']);
+        }
+        else
+        {
+            $req = "update meca
+                    set
+            meca_nom = :meca_nom,
+            meca_type = :meca_type,
+            meca_pos_etage = :meca_pos_etage,
+            meca_pos_type_aff = :meca_pos_type_aff,
+            meca_pos_decor = :meca_pos_decor,
+            meca_pos_decor_dessus = :meca_pos_decor_dessus,
+            meca_pos_passage_autorise = :meca_pos_passage_autorise,
+            meca_pos_modif_pa_dep = :meca_pos_modif_pa_dep,
+            meca_pos_ter_cod = :meca_pos_ter_cod,
+            meca_mur_type = :meca_mur_type,
+            meca_mur_tangible = :meca_mur_tangible,
+            meca_mur_illusion = :meca_mur_illusion,
+            meca_si_active = :meca_si_active,
+            meca_si_desactive = :meca_si_desactive                        where meca_cod = :meca_cod ";
+            $stmt = $pdo->prepare($req);
+            $stmt = $pdo->execute(array(
+                ":meca_cod" => $this->meca_cod,
+                ":meca_nom" => $this->meca_nom,
+                ":meca_type" => $this->meca_type,
+                ":meca_pos_etage" => $this->meca_pos_etage,
+                ":meca_pos_type_aff" => $this->meca_pos_type_aff,
+                ":meca_pos_decor" => $this->meca_pos_decor,
+                ":meca_pos_decor_dessus" => $this->meca_pos_decor_dessus,
+                ":meca_pos_passage_autorise" => $this->meca_pos_passage_autorise,
+                ":meca_pos_modif_pa_dep" => $this->meca_pos_modif_pa_dep,
+                ":meca_pos_ter_cod" => $this->meca_pos_ter_cod,
+                ":meca_mur_type" => $this->meca_mur_type,
+                ":meca_mur_tangible" => $this->meca_mur_tangible,
+                ":meca_mur_illusion" => $this->meca_mur_illusion,
+                ":meca_si_active" => $this->meca_si_active,
+                ":meca_si_desactive" => $this->meca_si_desactive,
+            ),$stmt);
+        }
+    }
+    /**
+     * Retourne un tableau de tous les enregistrements
+     * @global bdd_mysql $pdo
+     * @return \meca
+     */
+    function  getAll()
+    {
+        $retour = array();
+        $pdo = new bddpdo;
+        $req = "select meca_cod  from meca order by meca_cod";
+        $stmt = $pdo->query($req);
+        while($result = $stmt->fetch())
+        {
+            $temp = new meca;
+            $temp->charge($result["meca_cod"]);
+            $retour[] = $temp;
+            unset($temp);
+        }
+        return $retour;
+    }
+
+    public function __call($name, $arguments){
+        switch(substr($name, 0, 6)){
+            case 'getBy_':
+                if(property_exists($this, substr($name, 6)))
+                {
+                    $retour = array();
+                    $pdo = new bddpdo;
+                    $req = "select meca_cod  from meca where " . substr($name, 6) . " = ? order by meca_cod";
+                    $stmt = $pdo->prepare($req);
+                    $stmt = $pdo->execute(array($arguments[0]),$stmt);
+                    while($result = $stmt->fetch())
+                    {
+                        $temp = new meca;
+                        $temp->charge($result["meca_cod"]);
+                        $retour[] = $temp;
+                        unset($temp);
+                    }
+                    if(count($retour) == 0)
+                    {
+                        return false;
+                    }
+                    return $retour;
+                }
+                else
+                {
+                    die('Unknown variable ' . substr($name, 6) . ' in table meca');
+                }
+                break;
+
+            default:
+                ob_start();
+                debug_print_backtrace();
+                $out = ob_get_contents();
+                error_log($out);
+                die('Unknown method.');
+        }
+    }
+}
--- web/www/jeu_test/admin_meca_etage.php
+++ web/www/jeu_test/admin_meca_etage.php
@@ -30,14 +30,72 @@ if ($erreur == 0)
 
     //-- traitement des actions=======================================================================================
     //print_r($_REQUEST);
-    if(isset($_POST['methode']) && $_POST['methode']=="creer_meca")
+    if(isset($_POST['methode']) && ($pos_etage>1) && isset($_POST['methode']))
     {
-        print_r($_POST);
+        //print_r($_POST);
 
-        writelog($log . $message, 'lieux_etages');
-        echo nl2br($message);
-        echo "<hr>";
+        if ($_POST['methode']=="editer_meca" ) {
 
+            $meca_nom = $_POST["nom"] == "" ? "Inconnu" : $_POST["nom"] ;
+            $bindings = array(
+                ":meca_nom"                     => $meca_nom,
+                ":meca_type"                    => $_POST["meca_type"] == "" ? "G" : $_POST["meca_type"],
+                ":meca_pos_etage"               => $pos_etage,
+                ":meca_pos_type_aff"            => ($_POST["type_aff"] == "" || $_POST["type_aff"] == "-1") ? NULL : $_POST["type_aff"],
+                ":meca_pos_decor"               => (int)$_POST["pos_decor"] == 0 ? NULL : $_POST["pos_decor"],
+                ":meca_pos_decor_dessus"        => (int)$_POST["decor_dessus"] == 0 ? NULL : $_POST["decor_dessus"],
+                ":meca_pos_passage_autorise"    => $_POST["passage_autorise"] == "-1" ? NULL : ($_POST["passage_autorise"] == "0" ? 1 : 0),
+                ":meca_pos_modif_pa_dep"        => $_POST["modif_pa_dep"] == "" ? NULL : $_POST["modif_pa_dep"],
+                ":meca_pos_ter_cod"             => $_POST["pos_ter_cod"] == "-1" ? NULL : $_POST["pos_ter_cod"],
+                ":meca_mur_type"                => (int)$_POST["mur_type"] == 0 ? NULL : $_POST["mur_type"],
+                ":meca_mur_tangible"            => $_POST["mur_tangible"] == "-1" ? NULL : ($_POST["mur_tangible"] == "0" ? 'N': 'O'),
+                ":meca_mur_illusion"            => $_POST["mur_illusion"] == "-1" ? NULL: ($_POST["mur_illusion"] == "0" ? 'N': 'O'),
+            );
+
+            if ($_POST['meca_cod']>0) {
+
+                $req = "UPDATE meca SET meca_nom=:meca_nom, meca_type=:meca_type, meca_pos_etage=:meca_pos_etage, meca_pos_type_aff=:meca_pos_type_aff, 
+                                        meca_pos_decor=:meca_pos_decor, meca_pos_decor_dessus=:meca_pos_decor_dessus, meca_pos_passage_autorise=:meca_pos_passage_autorise, 
+                                        meca_pos_modif_pa_dep=:meca_pos_modif_pa_dep, meca_pos_ter_cod=:meca_pos_ter_cod, meca_mur_type=:meca_mur_type, 
+                                        meca_mur_tangible=:meca_mur_tangible,meca_mur_illusion=:meca_mur_illusion WHERE meca_cod=:meca_cod ";
+
+                $bindings = array_merge($bindings, array( ":meca_cod" => $_POST['meca_cod']));
+
+                $log =date("d/m/y - H:i") . $perso->perso_nom . " (compte $compt_cod) modifie des mécanismes d'étage : $pos_etage\n";
+                $message = "Modification du mécanisme: #".  $_POST['meca_cod']." (".$meca_nom.")";
+
+            } else {
+
+                $req = "INSERT INTO meca (meca_nom, meca_type, meca_pos_etage, meca_pos_type_aff, meca_pos_decor, meca_pos_decor_dessus, meca_pos_passage_autorise, meca_pos_modif_pa_dep, meca_pos_ter_cod, meca_mur_type, meca_mur_tangible, meca_mur_illusion)
+                            VALUES (:meca_nom, :meca_type, :meca_pos_etage, :meca_pos_type_aff, :meca_pos_decor, :meca_pos_decor_dessus, :meca_pos_passage_autorise, :meca_pos_modif_pa_dep, :meca_pos_ter_cod, :meca_mur_type, :meca_mur_tangible,:meca_mur_illusion)";
+
+                $log =date("d/m/y - H:i") . $perso->perso_nom . " (compte $compt_cod) modifie des mécanismes d'étage : $pos_etage\n";
+                $message = "Creation du mécanisme: ". $meca_nom;
+            }
+
+            $stmt = $pdo->prepare($req);
+            $stmt = $pdo->execute($bindings, $stmt);
+
+            writelog($log . $message, 'lieux_etages');
+            echo nl2br($message);
+            echo "<hr>";
+
+            unset($_POST['meca_cod'] );
+            unset($_REQUEST['meca_cod'] );
+
+        } else if ($_POST['methode']=="supprimer_meca") {
+
+                $req = "DELETE FROM meca where meca_cod=:meca_cod " ;
+                $stmt = $pdo->prepare($req);
+                $stmt = $pdo->execute(array(":meca_cod"  => $_POST['meca_cod'] ), $stmt);
+
+                $log =date("d/m/y - H:i") . $perso->perso_nom . " (compte $compt_cod) supprime mécanismes d'étage : $pos_etage\n";
+                $message = "Suppression du mécanisme: #". $_POST['meca_cod'];
+
+                writelog($log . $message, 'lieux_etages');
+                echo nl2br($message);
+                echo "<hr>";
+        }
     }
 
     //=======================================================================================
@@ -87,6 +145,10 @@ if ($erreur == 0)
     if ($pos_etage==0) {
         echo 'Sélectionnez l’étage!';
     } else {
+
+        $meca = new meca();
+        if (isset($_REQUEST["meca_cod"]) && $_REQUEST["meca_cod"]>0) { $meca->charge($_REQUEST["meca_cod"]); }
+
         echo "<div class=\"bordiv\">
             <table>
                 <tr>
@@ -105,55 +167,114 @@ if ($erreur == 0)
             </table></div>
         ";
 
-        $req_m_terrain= "select ter_cod, ter_nom from terrain where ter_cod > 0 order by ter_nom";
+        $req_m_terrain = "select ter_cod, ter_nom from terrain where ter_cod > 0 order by ter_nom";
         $stmt_m_terrain = $pdo->query($req_m_terrain);
         $terrains = $stmt_m_terrain->fetchAll(PDO::FETCH_ASSOC);
 
         echo "
         <form method='post' action='admin_meca_etage.php'>
-                    <input type='hidden' name='methode' value='creer_meca'>
+                    <input type='hidden' name='methode' value='editer_meca'>
                     <input type='hidden' name='pos_etage' value='$pos_etage'>
-                    <input type='hidden' id='meca-Fond' name='type_aff' value=''>
-                    <input type='hidden' id='meca-Decor' name='pos_decor' value=''>
-                    <input type='hidden' id='meca-Mur' name='mur_type' value=''>
-                    <input type='hidden' id='meca-DecorDessus' name='decor_dessus' value=''>
+                    <input type='hidden' name='meca_cod' value='{$meca->meca_cod}'>
+                    <input type='hidden' id='meca-Fond' name='type_aff' value='".$meca->meca_pos_type_aff."'>
+                    <input type='hidden' id='meca-Decor' name='pos_decor' value='".$meca->meca_pos_decor."'>
+                    <input type='hidden' id='meca-Mur' name='mur_type' value='".$meca->meca_mur_type."'>
+                    <input type='hidden' id='meca-DecorDessus' name='decor_dessus' value='".$meca->meca_pos_decor_dessus."'>
                     
-            <br>Nom du mécanisme: <input type='text' name='nom'  size='40'><br/>
-            <div style='float: left; display: inline-flex;'>Pattern:&nbsp;&nbsp;&nbsp; 
-                        <div id='pattern-Fond' data-pattern='0' class='caseVue caseFond pinceau'>
-                            <div id='pattern-Decor' data-pattern='0' class='caseVue'>
-                                <div id='pattern-Mur' data-pattern='0' class='caseVue'>                                
-                                    <div id='pattern-DecorDessus' data-pattern='0' class='caseVue'>                                
+            <br>Nom du mécanisme: <input type='text' name='nom'  size='40' value='".$meca->meca_nom."'><br>
+            <br>Type de mécanisme:";
+
+        echo '<select name="meca_type" id="select-type">';
+        echo '<option '.($meca->meca_type=="G" ? "selected" : "").' value="G">Grappe</option>';
+        echo '<option '.($meca->meca_type=="I" ? "selected" : "").' value="I">Individuel</option>';
+        echo '</select> (<em style="font-size: 10px;">Si grappe est choisi, à l’activation de ce mécanisme, les cases l’utilisant seront toutes activées. Sinon c’est seulement la case du perso</em>)<br>';
+
+        $chkp0 = "";  $chkp1 = "" ;   $chkp2 = "" ;  if ($meca->meca_pos_passage_autorise=="1") {$chkp1 = " checked ";} else if ($meca->meca_pos_passage_autorise=="0") {  $chkp2 = " checked " ; } else {  $chkp0 = " checked " ; };
+        $chkmt0 = "";  $chkmt1 = "" ;   $chkmt2 = "" ;  if ($meca->meca_mur_tangible=="N") {$chkmt1 = " checked ";} else if ($meca->meca_mur_tangible=="O") {  $chkmt2 = " checked " ; } else {  $chkmt0 = " checked " ; };
+        $chkmi0 = "";  $chkmi1 = "" ;   $chkmi2 = "" ;  if ($meca->meca_mur_illusion=="N") {$chkmi1 = " checked ";} else if ($meca->meca_mur_illusion=="O") {  $chkmi2 = " checked " ; } else {  $chkmi0 = " checked " ; };
+
+        echo "<div style='float: left; display: inline-flex;'>Pattern:&nbsp;&nbsp;&nbsp; 
+                        <div id='pattern-Fond' data-pattern='".($meca->meca_pos_type_aff=="" ? -1 : $meca->meca_pos_type_aff)."' class='caseVue caseFond pinceau ".($meca->meca_pos_type_aff=="" ? "" : "v".$meca->meca_pos_type_aff)."'>
+                            <div id='pattern-Decor' data-pattern='".(int)$meca->meca_pos_decor."' class='caseVue ".($meca->meca_pos_decor=="" ? "" : "decor".$meca->meca_pos_decor)."'>
+                                <div id='pattern-Mur' data-pattern='".(int)$meca->meca_mur_type."' class='caseVue ".($meca->meca_mur_type=="" ? "" : "mur".$meca->meca_pos_decor)."'>                                
+                                    <div id='pattern-DecorDessus' data-pattern='".(int)$meca->meca_pos_decor_dessus."' class='caseVue ".($meca->meca_pos_decor_dessus=="" ? "" : "decor".$meca->meca_pos_decor_dessus)."'>                                
                                     </div>
                                 </div>
                             </div>
                         </div>
             </div><br/><br>
             <table>
-            <tr><td>Passage autorisé:</td><td><input checked type='radio' name='passage_autorise' value='-1'>Base</td><td>&nbsp;|&nbsp;<input type='radio' name='passage_autorise' value='0'>Autorisé</td><td>&nbsp;|&nbsp;<input type='radio' name='passage_autorise' value='1'>Interdit<td><tr></tr>
-            <tr><td>Mur tangible:</td><td><input checked type='radio' name='mur_tangible' value='-1'>Base</td><td>&nbsp;|&nbsp;<input type='radio' name='mur_tangible' value='0'>Tangible</td><td>&nbsp;|&nbsp;<input type='radio' name='mur_tangible' value='1'>Intangible<td><tr></tr>
-            <tr><td>Mur illusion:</td><td><input checked type='radio' name='mur_illusion' value='-1'>Base</td><td>&nbsp;|&nbsp;<input type='radio' name='mur_illusion' value='0'>Infranchissable</td><td>&nbsp;|&nbsp;<input type='radio' name='mur_illusion' value='1'>Illusion<td><tr>
+            <tr><td>Passage autorisé:</td><td><input $chkp0 type='radio' name='passage_autorise' value='-1'>Base</td><td>&nbsp;|&nbsp;<input $chkp1 type='radio' name='passage_autorise' value='0'>Autorisé</td><td>&nbsp;|&nbsp;<input $chkp2 type='radio' name='passage_autorise' value='1'>Interdit<td><tr></tr>
+            <tr><td>Mur tangible:</td><td><input $chkmt0 type='radio' name='mur_tangible' value='-1'>Base</td><td>&nbsp;|&nbsp;<input $chkmt1 type='radio' name='mur_tangible' value='0'>Tangible</td><td>&nbsp;|&nbsp;<input $chkmt2 type='radio' name='mur_tangible' value='1'>Intangible<td><tr></tr>
+            <tr><td>Mur illusion:</td><td><input $chkmi0 type='radio' name='mur_illusion' value='-1'>Base</td><td>&nbsp;|&nbsp;<input $chkmi1 type='radio' name='mur_illusion' value='0'>Infranchissable</td><td>&nbsp;|&nbsp;<input $chkmi2 type='radio' name='mur_illusion' value='1'>Illusion<td><tr>
             </table>
             Terrain: ";
-            echo '<select name="pos_ter_cod" id="select-terrain">';
-            echo '<option value="-1">Terrain Inchangé (base)</option>';
-            echo '<option value="0">Sans terrain spécifique</option>';
-            for ($t=0; $t<count($terrains); $t++ )
-            {
-                echo '<option value="'.$terrains[$t]["ter_cod"].'">'.$terrains[$t]["ter_nom"].'</option>';
-            }
-            echo '</select> Modificateur de PA: <input name="modif_pa_dep" id="modif_pa_dep" value="" type="text" size="3"/> (<em style="font-size: 10px;">laisser vide pour garder le modificateur de base</em>)';
-        echo "<br><br><input type='submit' class='test' value='créer le mécanisme!'/><br>
-        </form>";
+        echo '<select name="pos_ter_cod" id="select-terrain">';
+        echo '<option '.($meca->meca_pos_ter_cod=="" ? "selected ": "").' value="-1">Terrain Inchangé (base)</option>';
+        echo '<option '.($meca->meca_pos_ter_cod=="0" ? "selected ": "").'value="0">Sans terrain spécifique</option>';
+        for ($t = 0; $t < count($terrains); $t++)
+        {
+            echo '<option '.($meca->meca_pos_ter_cod==$terrains[$t]["ter_cod"] ? "selected ": "").' value="' . $terrains[$t]["ter_cod"] . '">' . $terrains[$t]["ter_nom"] . '</option>';
+        }
+        echo '</select> Modificateur de PA: <input name="modif_pa_dep" id="modif_pa_dep" value="'.$meca->meca_pos_modif_pa_dep.'" type="text" size="3"/> (<em style="font-size: 10px;">laisser vide pour garder le modificateur de base</em>)';
 
+        $bouton = ($meca->meca_cod > 0) ? "Modifier le mécanisme!" : "créer le mécanisme!";
+        echo "<br><br><input type='submit' class='test' value='{$bouton}'/>";
 
+        if ($meca->meca_cod > 0)
+        {
+            echo '&nbsp;&nbsp;&nbsp;&nbsp;ou&nbsp;<a href="admin_meca_etage.php?pos_etage='.$pos_etage.'">Créer un nouveau</a><br>';
+        }
+        echo "<br></form>";
 
         echo "<HR>Liste des mécanismes de l'étage:";
 
+        echo " <form name='supprimermeca' method='post' action='admin_meca_etage.php'>
+                   <input type='hidden' name='methode' value='supprimer_meca'>
+                   <input type='hidden' name='pos_etage' value='{$pos_etage}'>
+                   <input type='hidden' name='meca_cod' value=''>
+               </form>
+               <script language='javascript'>
+                   function supprimermeca(code)
+                   {
+                       document.supprimermeca.meca_cod.value = code;
+                       document.supprimermeca.submit();
+                   }
+               </script>
+               <table>";
 
+        $req_meca  = "select * from meca left join terrain on ter_cod=meca_pos_ter_cod where meca_pos_etage = ? order by meca_nom, meca_cod";
+        $stmt = $pdo->prepare($req_meca);
+        $stmt = $pdo->execute(array($pos_etage), $stmt);
+        echo "<tr><td></td><td width='200px;'><b>Nom</b></td><td><b>Type</b></td><td><b>Pattern</b></td><td><b>Passage</b></td><td><b>Mur Tangible</b></td><td><b>Mur Illusion</b></td><td><b>Terrain</b></td><td><b>Modif. PA</b></td><td></td></tr>";
+        while ($result = $stmt->fetch())
+        {
+            echo "
+            <tr>
+                <td><a href=\"?pos_etage={$pos_etage}&meca_cod={$result['meca_cod']}\">Modifier</a></td>
+                <td>{$result['meca_nom']}</td>
+                <td><div class='caseVue caseFond pinceau ".($result['meca_pos_type_aff']=="" ? "" : "v".$result['meca_pos_type_aff'])."'>
+                            <div class='caseVue ".($result['meca_pos_decor']=="" ? "" : "decor".$result['meca_pos_decor'])."'>
+                                <div  class='caseVue ".($result['meca_mur_type']=="" ? "" : "mur".$result['meca_mur_type'])."'>                                
+                                    <div class='caseVue ".($result['meca_pos_decor_dessus']=="" ? "" : "decor".$result['meca_pos_decor_dessus'])."'>                                
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </td>
+                <td>".($result['meca_type']=="G" ? "Grappe" : "Individuel")."</td>
+                <td>".($result['meca_pos_passage_autorise']=="" ? "base" : ($result['meca_pos_passage_autorise']=="1" ? "autorisé": "interdit"))."</td>
+                <td>".($result['meca_mur_tangible']=="" ? "base" : ($result['meca_pos_passage_autorise']=="O" ? "intangible": "tagible"))."</td>
+                <td>".($result['meca_mur_illusion']=="" ? "base" : ($result['meca_mur_illusion']=="O" ? "illusion": "infranchissable"))."</td>
+                <td>".($result['meca_pos_ter_cod']=="" ? "base" : ($result['meca_pos_ter_cod']=="0" ? "sans terrain": $result['ter_nom']))."</td>
+                <td>".($result['meca_pos_modif_pa_dep']=="" ? "base" : (int)$result['meca_pos_modif_pa_dep'] )."</td>
+                <td><a href=\"javascript:supprimermeca({$result['meca_cod']});\">Supprimer</a></td>
+            </tr>";
+        }
+        echo '</table>';
     }
 }
-
+#meca_nom, meca_type, meca_pos_etage, meca_pos_type_aff, meca_pos_decor, meca_pos_decor_dessus, meca_pos_passage_autorise, meca_pos_modif_pa_dep, meca_pos_ter_cod, meca_mur_type, meca_mur_tangible, meca_mur_illusion)
 
 //=======================================================================================
 // == Footer
