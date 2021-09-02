3644b8573438f0c46e5579d455204d61f1082064
Marlyza <maria.lyzzie@gmail.com>
2021-09-02 16:26:50 +0200

    ea_mouv: nouveaux terrains et design

---
+++ sql/20210902_0001.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO public.terrain( ter_cod, ter_nom, ter_desc, ter_msg_inaccessible) VALUES
+      (15, 'toxic', 'toxic', 'Les émanations toxiques du terrain infecte [cible].'),
+      (16, 'altitude', 'altitude', 'Ici, c''est beaucoup trop haut pour [cible], une chute serait mortelle.') ;
