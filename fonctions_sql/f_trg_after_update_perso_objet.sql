8f5ecbf4698cce29532c5889a3cc88805d2faf13
Marlyza <maria.lyzzie@gmail.com>
2021-10-14 19:04:13 +0200

    ea_mouv: objet a BM: utilisation en non-équipé (step 2)

--- fonctions_sql/f_trg_after_update_perso_objet.sql
+++ fonctions_sql/f_trg_after_update_perso_objet.sql
@@ -26,7 +26,7 @@ begin
 
     -- boucle sur tous les BM donné par cet objet
     for ligne in
-      select objbm_cod, tbonus_libc, objbm_bonus_valeur, objbm_equip_requis from objets_bm join bonus_type on tbonus_cod=objbm_tbonus_cod where objbm_gobj_cod=v_gobj_cod or objbm_obj_cod=NEW.perobj_obj_cod and objbm_equip_requis
+      select objbm_cod, tbonus_libc, objbm_bonus_valeur, objbm_equip_requis from objets_bm join bonus_type on tbonus_cod=objbm_tbonus_cod where (objbm_gobj_cod=v_gobj_cod or objbm_obj_cod=NEW.perobj_obj_cod) and objbm_equip_requis
     loop
 
         if NEW.perobj_equipe = 'N' then
@@ -42,29 +42,6 @@ begin
         end if;
 
     end loop;
-
-
-/*
-
-    if NEW.perobj_equipe = 'N' then
-
-        -- retirer les bonus d'équipement de l'objet !!!
-        perform retire_bonus_equipement(NEW.perobj_perso_cod, NEW.perobj_obj_cod);
-
-    else
-
-      -- on a équipé un objet, ajouter des bonus d'équipement s'il y en a
-      for ligne in
-        select objbm_cod, tbonus_libc, objbm_bonus_valeur from objets_bm join bonus_type on tbonus_cod=objbm_tbonus_cod where objbm_gobj_cod=v_gobj_cod or objbm_obj_cod=NEW.perobj_obj_cod
-      loop
-        -- ajout des bonus
-        perform ajoute_bonus_equipement(NEW.perobj_perso_cod, ligne.tbonus_libc, ligne.objbm_cod, NEW.perobj_obj_cod, ligne.objbm_bonus_valeur);
-
-      end loop;
-
-    end if;
-  */
-
 
   end if;
 
