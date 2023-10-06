69b99e1d26a2983b6b19f58a350cd280fadfca60
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2023-10-06 17:05:21 +0200

    mise à jour de l'ia des monture pour les super-monture

---
+++ sql/20231006_0001_ia_monture.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO type_ia( ia_type, ia_nom, ia_fonction) VALUES ( 20, 'Monture Super (BipBip)', 'ia_monture([perso], 3)');
+
+INSERT INTO type_ia( ia_type, ia_nom, ia_fonction) VALUES ( 21, 'Monture Super (Coyote)', 'ia_monture([perso], 4)');
