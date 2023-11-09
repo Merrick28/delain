d2ef6a97f8c2366f6c10b4f25294649dc76d2e3c
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2023-11-09 11:41:33 +0100

    QA reparation wording

--- sql/20231107_00001_qa.sql
+++ sql/20231107_00001_qa.sql
@@ -8,7 +8,7 @@ INSERT INTO quetes.aquete_etape_modele(  aqetapmodel_tag, aqetapmodel_nom, aqeta
             '[1:delai|1%1],[2:perso|1%0],[3:type_objet|1%0],[4:objet_generique|1%0],[5:valeur|1%1]',
             'Délai alloué pour cette étape.|' ||
             'Le joueur doit se trouver sur la case de ce PNJ pour faire la réparation.|' ||
-            'Liste des types d''objet réparable (0 pour tous les type)|' ||
-            'Liste des generiques d''objet réparable (0 pour tous)|' ||
-            'Tarif des réparations en Bzf par état à réparer',
+            'Liste des types d''objet réparable (0 pour tous les types)|' ||
+            'Liste des generiques d''objet réparable (0 pour tous génériques)|' ||
+            'Tarif des réparations en Bzf (pour chaque 1% de casse)',
             'Choisisez un objet, [2] vous le réparera pour le prix indiqué');
