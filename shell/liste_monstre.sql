61be1a36b6360cd26aef3e2aabb0500b314e42f6
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2024-04-09 16:58:16 +0200

    retour au script original depuis étage des zongos purgé

--- shell/liste_monstre.sql
+++ shell/liste_monstre.sql
@@ -6,10 +6,6 @@ select perso_cod from (
   and perso_der_connex + ((perso_temps_tour/2)::text || ' minutes')::interval < now()
   and (perso_dlt < now() or perso_pa >= 4)
   and (perso_dirige_admin != 'O' or perso_dirige_admin is null)
-  and perso_cod not in (
-	  		select perso_cod from perso,perso_position, positions
-			where  pos_cod=ppos_pos_cod and perso_cod=ppos_perso_cod and pos_etage=162 and perso_type_perso=2 and perso_actif='O'
-	   )
 union
   select m.perso_cod, 1 as priorite from perso p join perso m on m.perso_cod=p.perso_monture
   where m.perso_type_perso = 2 and p.perso_type_perso = 1
