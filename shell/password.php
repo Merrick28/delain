1f1fadbcefe195650fd0b245b011f9c447454d6e
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2025-06-21 12:34:00 +0200

    bugfix: passage a php8 2eme parametre de crypt requis

--- shell/password.php
+++ shell/password.php
@@ -7,6 +7,6 @@ if (!$compte->charge($argv[1]))
 {
     die('Erreur sur chargement de compte' . PHP_EOL);
 }
-$compte->compt_passwd_hash = crypt($argv[2]);
+$compte->compt_passwd_hash = crypt($argv[2], sha1(microtime(true)));
 $compte->stocke();
 echo "Le mot de passe du compte " . $compte->compt_nom . " a été changé" . PHP_EOL;
