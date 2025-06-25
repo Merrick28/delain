baafd2b83471d58718dfffdfd7af50daa3fb1715
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2025-06-25 14:22:41 +0200

    ajout nouveau type d'objet jambiere #47

---
+++ sql/20250625_0001_type_obj_jambiere.sql
@@ -0,0 +1,2 @@
+INSERT INTO type_objet( tobj_libelle, tobj_ident_comp_cod, tobj_max_equip, tobj_chute, tobj_nettoyage, tobj_identifie_auto, tobj_equipable, tobj_affichage_char, tobj_degradation)
+    VALUES ('Jambière', 3,  1, 10, 25, 0, 1, ']', 0);
