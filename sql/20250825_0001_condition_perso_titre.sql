fe5b3e7a75e12256baee3bfe61dacd1232bac36e
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2025-08-25 15:59:57 +0200

    ajout de condition perso sur les titres du perso

--- sql/20250825_0001_condition_perso_titre.sql
+++ sql/20250825_0001_condition_perso_titre.sql
@@ -1,2 +1,2 @@
 insert into quetes.aquete_type_carac (aqtypecarac_cod, aqtypecarac_nom, aqtypecarac_type, aqtypecarac_aff)
-values(39, 'Possède le titre', 'VARIABLE', 'Vérification d''un titre possédé par le perso');
+values(39, 'Titres obtenus', 'VARIABLE', 'Vérification d''un titre possédé par le perso');
