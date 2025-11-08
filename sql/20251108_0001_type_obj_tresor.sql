93b9d74c76bd38657d951782707b6aba8f44fab2
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2025-11-08 11:35:31 +0100

    ajout nouveau type objet: tresor, ceinture, sceau

---
+++ sql/20251108_0001_type_obj_tresor.sql
@@ -0,0 +1,8 @@
+INSERT INTO type_objet( tobj_libelle, tobj_ident_comp_cod, tobj_max_equip, tobj_chute, tobj_nettoyage, tobj_identifie_auto, tobj_equipable, tobj_affichage_char, tobj_degradation)
+VALUES ('Trésor', null,  3, 10, null, 1, 1, '+', 0);
+
+INSERT INTO type_objet( tobj_libelle, tobj_ident_comp_cod, tobj_max_equip, tobj_chute, tobj_nettoyage, tobj_identifie_auto, tobj_equipable, tobj_affichage_char, tobj_degradation)
+VALUES ('Ceinture', null,  1, 10, 25, 1, 1, ']', 60);
+
+INSERT INTO type_objet( tobj_libelle, tobj_ident_comp_cod, tobj_max_equip, tobj_chute, tobj_nettoyage, tobj_identifie_auto, tobj_equipable, tobj_affichage_char, tobj_degradation)
+VALUES ('Sceau', null,  1, 10, null, 1, 1, '+', 0);
