d3449203238518a65f8c50ffa4ae2e6b2459fea0
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2026-09-08 20:20:24 +0200

    amelioration interface admin monstre (objets depuis modèle)

--- web/www/jeu_test/admin_traitement_type_monstre_edit.php
+++ web/www/jeu_test/admin_traitement_type_monstre_edit.php
@@ -1190,6 +1190,53 @@ switch ($methode)
         echo "Modification des objets : $nb_modifications modifié(s), $nb_suppressions supprimé(s)";
         break;
 
+    case "replace_mon_drops":
+
+        $gmon_source_cod = (int)$_POST['gmon_source_cod'];
+        if ($gmon_source_cod <= 0) {
+            echo "Aucun monstre source sélectionné";
+            break;
+        }
+
+        if ($gmon_source_cod == $gmon_cod) {
+            echo "Impossible de copier sur lui-même";
+            break;
+        }
+
+        $pdo->query("delete from objets_monstre_generique  where ogmon_gmon_cod = $gmon_cod ");
+        $pdo->query("insert into objets_monstre_generique (ogmon_gmon_cod, ogmon_gobj_cod, ogmon_chance, ogmon_equipe)
+                            select $gmon_cod, ogmon_gobj_cod, ogmon_chance, ogmon_equipe
+                                from objets_monstre_generique
+                                where ogmon_gmon_cod = $gmon_source_cod ");
+
+        writelog( $log . "Remplacement des objets depuis le monstre $gmon_source_cod\n", 'monstre_edit');
+        echo "Objets remplacés";
+        break;
+
+    case "merge_mon_drops":
+
+        $gmon_source_cod = (int)$_POST['gmon_source_cod'];
+        if ($gmon_source_cod <= 0) {
+            echo "Aucun monstre source sélectionné";
+            break;
+        }
+
+        if ($gmon_source_cod == $gmon_cod) {
+            echo "Impossible de copier sur lui-même";
+            break;
+        }
+
+        $pdo->query("insert into objets_monstre_generique ( ogmon_gmon_cod, ogmon_gobj_cod, ogmon_chance, ogmon_equipe )
+                            select $gmon_cod, src.ogmon_gobj_cod, src.ogmon_chance, src.ogmon_equipe
+                                from objets_monstre_generique src
+                                where src.ogmon_gmon_cod = $gmon_source_cod
+                                     and not exists ( select 1 from objets_monstre_generique dest where dest.ogmon_gmon_cod = $gmon_cod and dest.ogmon_gobj_cod = src.ogmon_gobj_cod) ");
+
+        writelog($log . "Fusion des objets depuis le monstre $gmon_source_cod\n",'monstre_edit');
+        echo "Objets fusionnés";
+
+        break;
+
     case "add_mon_fonction":
 
         // Sauvegarder les modifications des effets-auto => save_effet_auto($post, $fonc_gmon_cod, $fonc_perso_cod)
--- web/www/jeu_test/admin_type_monstre_edit.php
+++ web/www/jeu_test/admin_type_monstre_edit.php
@@ -1500,6 +1500,23 @@ if ($erreur == 0)
                             <td colspan="2"></td>
                         </form>
                     </tr>
+                    <TR>
+                        <td colspan="6" align="left">
+                            <form method="post" action="admin_type_monstre_edit.php#section_objets">
+                                <input type="hidden" name="section" value="section_objets">
+                                <input type="hidden" name="methode2" value="edit">
+                                <input type="hidden" name="sel_method" value="edit">
+                                <input type="hidden" name="gmon_cod" value="<?php echo $gmon_cod ?>">
+
+                                <strong>Importer les objets depuis un autre monstre générique :</strong>
+                                <input name="gmon_source_cod"  id="drop_gmon_source_cod" type="text" size="5" value="">
+                                <span id="drop_gmon_source_nom"></span>
+                                <input type="button" class="test" value="Rechercher" onClick='getTableCod("drop_gmon_source","monstre_generique","Rechercher un monstre générique");'>
+                                &nbsp;&nbsp;&nbsp;&nbsp;<button type="submit" name="methode" value="replace_mon_drops"> Remplacer </button>
+                                &nbsp;&nbsp;&nbsp;&nbsp;<button type="submit" name="methode" value="merge_mon_drops"> Fusionner </button>
+                            </form>
+                        </td>
+                    </TR>
                 </TABLE>
                 <!-- ======================================================= MONTURE ======================================================= -->
                 <hr>
